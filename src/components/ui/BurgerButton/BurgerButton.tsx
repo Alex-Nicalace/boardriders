347a63985652a6f309235d7712a7e984768d7032
Alex-Nicalace <anicalace@gmail.com>
2023-12-18 13:25:49 +0200

    added BurgerButton

---
+++ src/components/ui/BurgerButton/BurgerButton.tsx
@@ -0,0 +1,11 @@
+import './BurgerButton.scss';
+
+function BurgerButton(): JSX.Element {
+  return (
+    <button className="burger">
+      <span></span>
+    </button>
+  );
+}
+
+export default BurgerButton;
