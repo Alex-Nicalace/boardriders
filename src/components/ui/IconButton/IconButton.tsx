bc34682267e1872ff34c14f852d0cffb6a1fd7b5
Alex-Nicalace <anicalace@gmail.com>
2023-12-18 13:26:53 +0200

    renamed LincIcon yo IconButton

---
+++ src/components/ui/IconButton/IconButton.tsx
@@ -0,0 +1,41 @@
+import { Link } from 'react-router-dom';
+import './IconButton.scss';
+import MediaQuery from '../MediaQuery';
+
+interface ILinkIconProps {
+  children?: React.ReactNode;
+  className?: string;
+  IconComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & {
+      title?: string | undefined;
+    }
+  >;
+  to?: string;
+}
+
+function IconButton({
+  children,
+  className,
+  IconComponent,
+  to,
+}: ILinkIconProps): JSX.Element {
+  const inner = (
+    <>
+      <IconComponent className="icon-button__icon" />
+      <MediaQuery minWidth="tablet">
+        <span className="icon-button__text">{children}</span>
+      </MediaQuery>
+    </>
+  );
+  if (!to) {
+    return <button className={`${className} icon-button`}>{inner}</button>;
+  }
+
+  return (
+    <Link to={to} className={`${className} icon-button`}>
+      {inner}
+    </Link>
+  );
+}
+
+export default IconButton;
