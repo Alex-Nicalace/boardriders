f6cb308869ba131322486392a64370d0b3bd4c28
Alex-Nicalace <anicalace@gmail.com>
2024-03-20 17:09:29 +0200

    Breadcrumbs: styling

--- src/components/ui/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/ui/Breadcrumbs/Breadcrumbs.tsx
@@ -10,14 +10,27 @@ export type TBreadcrumbsData = [...Required<ITst>[], ITst];
 
 type TBreadcrumbsProps = {
   data: TBreadcrumbsData;
+  color?: 'black' | 'white';
+  className?: string;
 };
-function Breadcrumbs({ data }: TBreadcrumbsProps): JSX.Element {
+function Breadcrumbs({
+  data,
+  color,
+  className = '',
+}: TBreadcrumbsProps): JSX.Element {
+  const modificator = color === 'white' ? 'breadcrumbs_white' : '';
   return (
-    <nav className="breadcrumbs">
+    <nav className={`${className} breadcrumbs ${modificator}`}>
       <ListLinks
         linksData={data}
         renderToItem={({ to, title }) =>
-          to ? <Link to={to}>{title}</Link> : <span>{title}</span>
+          to ? (
+            <Link className="breadcrumbs__link" to={to}>
+              {title}
+            </Link>
+          ) : (
+            <span>{title}</span>
+          )
         }
         listProps={{ className: 'breadcrumbs__list' }}
         itemProps={{ className: 'breadcrumbs__item' }}
