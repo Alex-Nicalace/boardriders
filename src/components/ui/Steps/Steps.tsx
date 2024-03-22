e5b658e2c876d17dfcf3e62ff8c2e2ecc13d34a3
Alex-Nicalace <anicalace@gmail.com>
2024-03-22 12:49:28 +0200

    Steps: Create

---
+++ src/components/ui/Steps/Steps.tsx
@@ -0,0 +1,52 @@
+import { AnthropometricIcon, DynamicsIcon, MountainIcon } from '../Icons';
+import Step from '../Step';
+import StepQuestion from '../StepQuestion';
+import './Steps.scss';
+
+const STEPS = [
+  {
+    numStep: 1,
+    question: 'Укажите свой рост и вес',
+    answerNode: (
+      <>
+        <span>Рост: 190 см</span>
+        <span>Вес: 300кг</span>
+      </>
+    ),
+    iconElement: <AnthropometricIcon />,
+    isFilled: true,
+  },
+  {
+    numStep: 2,
+    question: 'Где планируете катать',
+    answerNode: 'Укажите место',
+    iconElement: <MountainIcon />,
+  },
+  {
+    numStep: 3,
+    question: 'Ваш уровень катании',
+    answerNode: 'Укажите ваш уровень',
+    iconElement: <DynamicsIcon />,
+  },
+];
+// type TStepsProps = { }
+function Steps(/*{ }: TStepsProps*/): JSX.Element {
+  return (
+    <div className="steps">
+      <Step className="steps__step">
+        <StepQuestion intro="Подбери борд" />
+      </Step>
+      {STEPS.map((step) => (
+        <Step
+          key={step.numStep}
+          className="steps__step"
+          isFilled={step.isFilled}
+        >
+          <StepQuestion {...step} />
+        </Step>
+      ))}
+    </div>
+  );
+}
+
+export default Steps;
