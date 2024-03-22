9f728d935b565945a787ce8fd017935ab35c7cf9
Alex-Nicalace <anicalace@gmail.com>
2024-03-22 12:50:52 +0200

    ProductCategoryFilterCards: append Steps

--- src/components/ProductCategoryFilterCards/ProductCategoryFilterCards.tsx
+++ src/components/ProductCategoryFilterCards/ProductCategoryFilterCards.tsx
@@ -1,4 +1,5 @@
 import Container from '../Container';
+import Steps from '../ui/Steps';
 import Title from '../ui/Title';
 
 // type TProductCategoryFilterCardsProps = {}
@@ -8,6 +9,7 @@ function ProductCategoryFilterCards(/*{ }: TProductCategoryFilterCardsProps*/): 
       <Title as="h2" kind="h1-32-h2-21" supNode="358">
         Сноуборд
       </Title>
+      <Steps />
     </Container>
   );
 }
