78a77692ee2c01f7e87b9b56a6ad7a335b1726e8
Alex-Nicalace <anicalace@gmail.com>
2024-03-25 16:25:14 +0200

    PromoSlider: warning uniqueness of the keys

--- src/components/PromoSlider/PromoSlider.tsx
+++ src/components/PromoSlider/PromoSlider.tsx
@@ -1,11 +1,11 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import Banner from '../ui/Banner';
 import './PromoSlider.scss';
+import Banner from '../ui/Banner';
+import PromoBlock from '../PromoBlock';
 
 import img1 from '../../assets/img/actions-slider/01.jpg';
 import img2 from '../../assets/img/actions-slider/02.jpg';
 import img3 from '../../assets/img/actions-slider/03.jpg';
-import PromoBlock from '../PromoBlock';
 
 const BANNERS_DATA = [
   {
@@ -22,21 +22,6 @@ const BANNERS_DATA = [
     img: img3,
     to: '#',
     title: 'Название акции №3',
-  },
-  {
-    img: img1,
-    to: '#',
-    title: 'Название акции №4',
-  },
-  {
-    img: img2,
-    to: '#',
-    title: 'Название акции №5',
-  },
-  {
-    img: img3,
-    to: '#',
-    title: 'Название акции №6',
   },
 ];
 
