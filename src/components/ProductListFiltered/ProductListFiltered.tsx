3a4e9f2cc86ac14fa5e385c7ec04d31948d1e9af
Alex-Nicalace <anicalace@gmail.com>
2024-03-31 13:01:19 +0300

    ProductListFiltered: added panel filters & products

--- src/components/ProductListFiltered/ProductListFiltered.tsx
+++ src/components/ProductListFiltered/ProductListFiltered.tsx
@@ -6,6 +6,8 @@ import randomString from '../../utils/randomString';
 import Select from '../../component-library/Select';
 import { SelectIcon } from '../ui/Icons';
 import useMatchMedia from '../../hooks/useMatchMedia';
+import WareCard from '../ui/WareCard';
+import { useEffect, useRef, useState } from 'react';
 
 const ID = 'sort_' + randomString();
 
@@ -32,16 +34,314 @@ const SORTS = [
   },
 ];
 
+const PATH = '/src/assets/img/products-new/';
+
+const PRODUCTS_DATA = [
+  {
+    wareId: '1',
+    imgMain: PATH + '01.png',
+    imgSecond: PATH + '01-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    to: '#',
+  },
+  {
+    wareId: '2',
+    imgMain: PATH + '02.png',
+    imgSecond: PATH + '02-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    newPrice: 17392,
+    discount: -50,
+    to: '#',
+  },
+  {
+    wareId: '3',
+    imgMain: PATH + '03.png',
+    imgSecond: PATH + '03-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    to: '#',
+  },
+  {
+    wareId: '4',
+    imgMain: PATH + '04.png',
+    imgSecond: PATH + '04-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    to: '#',
+  },
+  {
+    wareId: '5',
+    imgMain: PATH + '02.png',
+    imgSecond: PATH + '02-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    newPrice: 17392,
+    discount: -50,
+    to: '#',
+  },
+  {
+    wareId: '6',
+    imgMain: PATH + '03.png',
+    imgSecond: PATH + '03-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    to: '#',
+  },
+  {
+    wareId: '7',
+    imgMain: PATH + '04.png',
+    imgSecond: PATH + '04-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    to: '#',
+  },
+  {
+    wareId: '8',
+    imgMain: PATH + '02.png',
+    imgSecond: PATH + '02-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    newPrice: 17392,
+    discount: -50,
+    to: '#',
+  },
+  {
+    wareId: '9',
+    imgMain: PATH + '03.png',
+    imgSecond: PATH + '03-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    to: '#',
+  },
+  {
+    wareId: '10',
+    imgMain: PATH + '04.png',
+    imgSecond: PATH + '04-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    to: '#',
+  },
+  {
+    wareId: '11',
+    imgMain: PATH + '02.png',
+    imgSecond: PATH + '02-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    newPrice: 17392,
+    discount: -50,
+    to: '#',
+  },
+  {
+    wareId: '12',
+    imgMain: PATH + '03.png',
+    imgSecond: PATH + '03-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    to: '#',
+  },
+  {
+    wareId: '13',
+    imgMain: PATH + '04.png',
+    imgSecond: PATH + '04-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    to: '#',
+  },
+  {
+    wareId: '14',
+    imgMain: PATH + '02.png',
+    imgSecond: PATH + '02-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    newPrice: 17392,
+    discount: -50,
+    to: '#',
+  },
+  {
+    wareId: '15',
+    imgMain: PATH + '03.png',
+    imgSecond: PATH + '03-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    to: '#',
+  },
+  {
+    wareId: '16',
+    imgMain: PATH + '04.png',
+    imgSecond: PATH + '04-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    to: '#',
+  },
+  {
+    wareId: '17',
+    imgMain: PATH + '02.png',
+    imgSecond: PATH + '02-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    newPrice: 17392,
+    discount: -50,
+    to: '#',
+  },
+  {
+    wareId: '18',
+    imgMain: PATH + '03.png',
+    imgSecond: PATH + '03-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    to: '#',
+  },
+  {
+    wareId: '19',
+    imgMain: PATH + '04.png',
+    imgSecond: PATH + '04-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    to: '#',
+  },
+  {
+    wareId: '20',
+    imgMain: PATH + '02.png',
+    imgSecond: PATH + '02-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    newPrice: 17392,
+    discount: -50,
+    to: '#',
+  },
+  {
+    wareId: '21',
+    imgMain: PATH + '03.png',
+    imgSecond: PATH + '03-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    to: '#',
+  },
+  {
+    wareId: '22',
+    imgMain: PATH + '04.png',
+    imgSecond: PATH + '04-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    to: '#',
+  },
+  {
+    wareId: '23',
+    imgMain: PATH + '02.png',
+    imgSecond: PATH + '02-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    newPrice: 17392,
+    discount: -50,
+    to: '#',
+  },
+  {
+    wareId: '24',
+    imgMain: PATH + '03.png',
+    imgSecond: PATH + '03-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    to: '#',
+  },
+  {
+    wareId: '25',
+    imgMain: PATH + '04.png',
+    imgSecond: PATH + '04-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    to: '#',
+  },
+  {
+    wareId: '26',
+    imgMain: PATH + '02.png',
+    imgSecond: PATH + '02-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    newPrice: 17392,
+    discount: -50,
+    to: '#',
+  },
+  {
+    wareId: '27',
+    imgMain: PATH + '03.png',
+    imgSecond: PATH + '03-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    to: '#',
+  },
+  {
+    wareId: '28',
+    imgMain: PATH + '04.png',
+    imgSecond: PATH + '04-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    to: '#',
+  },
+];
+
 const MEDIAQUERIES = ['( max-width: 991.98px )'];
 
 type TProductListFilteredProps = { className?: string };
 function ProductListFiltered({
   className = '',
 }: TProductListFilteredProps): JSX.Element {
+  const [hideFilter, setHideFilter] = useState(false);
+  const sectionRef = useRef<HTMLDivElement>(null);
   const [isSmallerThanTablet] = useMatchMedia(MEDIAQUERIES);
 
+  function toggleHideFilter() {
+    setHideFilter((prev) => !prev);
+  }
+
+  useEffect(
+    function () {
+      const sectionEl = sectionRef.current;
+      if (!sectionEl) return;
+
+      sectionEl.classList.toggle(
+        'product-list-filtered_filters-hide',
+        hideFilter
+      );
+
+      return () => {
+        sectionEl.classList.remove('product-list-filtered_filters-hide');
+      };
+    },
+    [hideFilter]
+  );
+
   return (
-    <section className={`product-list-filtered ${className}`}>
+    <section ref={sectionRef} className={`product-list-filtered ${className}`}>
       <div className="product-list-filtered__container">
         <Title
           className="product-list-filtered__title"
@@ -59,21 +359,25 @@ function ProductListFiltered({
                 className="product-list-filtered__toolbar-toggle"
                 labelActive="Скрыть фильтры"
                 labelNotActive="Показать фильтры"
+                isActive={!hideFilter}
+                onClick={toggleHideFilter}
               />
-              <label htmlFor={ID} className="product-list-filtered__sort-label">
-                Сортировать по:
-              </label>
-              <Select
-                className="product-list-filtered__sort-select"
-                iconElement={<SelectIcon />}
-                id={ID}
-              >
-                {SORTS.map((sort) => (
-                  <Select.Option key={sort.value} value={sort.value}>
-                    {sort.text}
-                  </Select.Option>
-                ))}
-              </Select>
+              <div className="product-list-filtered__sort">
+                <span className="product-list-filtered__sort-label">
+                  <label htmlFor={ID}>Сортировать по:</label>
+                </span>
+                <Select
+                  className="product-list-filtered__sort-select"
+                  iconElement={<SelectIcon />}
+                  id={ID}
+                >
+                  {SORTS.map((sort) => (
+                    <Select.Option key={sort.value} value={sort.value}>
+                      {sort.text}
+                    </Select.Option>
+                  ))}
+                </Select>
+              </div>
             </>
           ) : (
             <>
@@ -93,9 +397,88 @@ function ProductListFiltered({
                 className="product-list-filtered__toolbar-toggle"
                 isNotShowIcon
                 labelActive="Фильтры"
+                isActive={!hideFilter}
+                onClick={toggleHideFilter}
               />
             </>
           )}
+        </div>
+        <div className="product-list-filtered__body">
+          <div
+            className="product-list-filtered__filters"
+            style={{ border: '1px solid red' }}
+          >
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis
+            quis, possimus rem hic, nam necessitatibus, magni fuga velit minus
+            aliquid eaque esse magnam voluptatibus libero suscipit quia
+            laboriosam! Sapiente, odit? Lorem ipsum, dolor sit amet consectetur
+            adipisicing elit. Perferendis quod itaque voluptatum asperiores,
+            ipsam quas ut voluptatem saepe aperiam sequi porro modi quae debitis
+            consectetur distinctio aut, maxime excepturi, accusamus magnam fuga
+            repellendus! Ipsam et eius, aspernatur, blanditiis qui harum quasi
+            nemo esse iusto voluptatibus id ipsa at iure tempora dolorem, quidem
+            sed. Eligendi labore alias commodi, nam, atque ratione a aliquam
+            nesciunt fugiat debitis dignissimos molestiae, quidem aliquid?
+            Nesciunt repellat placeat laborum illo nulla corrupti ipsam,
+            asperiores ad ullam adipisci et odio autem fuga vitae! Deleniti ab
+            perferendis, consequatur aliquam id quidem eveniet alias voluptates,
+            laudantium quos facere adipisci suscipit omnis, a aspernatur.
+            Quaerat nam nemo magnam quam consequatur numquam iusto, quidem nihil
+            doloremque vero molestiae ad veniam et ratione, ab eligendi aperiam
+            repellat at repellendus nisi neque in itaque? Dignissimos ipsam
+            natus perferendis asperiores! Eum dignissimos sit magnam architecto
+            molestiae, doloremque vero cupiditate perferendis omnis sed esse
+            deleniti. Quibusdam exercitationem a nobis esse inventore velit
+            nulla ipsam quasi quisquam, expedita explicabo ut excepturi autem
+            dolorum sit blanditiis. Quae voluptas dolores consequuntur illum
+            neque. Qui laudantium doloribus voluptatem sed nam quam nisi? In at
+            dicta veritatis, doloremque voluptatibus cumque et nobis deserunt
+            animi minima, nisi esse qui autem maxime temporibus voluptatem
+            perspiciatis pariatur molestias eveniet ex blanditiis dolor ipsum!
+            Assumenda tenetur necessitatibus repellat mollitia soluta quos non
+            odit. Quibusdam iure accusamus, amet sed ratione veniam enim
+            laboriosam? Necessitatibus, consequuntur. Accusantium vitae cum
+            iusto alias eaque nobis. Quibusdam illo molestiae placeat deleniti
+            autem recusandae corporis et veritatis! Consequatur iure tempora rem
+            et repellendus odit praesentium commodi nesciunt, neque magni,
+            voluptates vel nemo voluptas! Similique adipisci rem architecto odio
+            incidunt nobis quae doloribus numquam, quidem fuga saepe impedit
+            doloremque autem fugit ipsam facere eveniet perferendis veritatis,
+            dolore vel aliquam amet! Voluptates incidunt amet ad eum nemo sed,
+            at accusantium pariatur voluptas ratione maiores id blanditiis
+            tempora repellat ea hic consequatur adipisci quasi numquam.
+            Corrupti, necessitatibus ex. Facilis impedit eos sed, repellendus
+            corporis neque officia laboriosam, quibusdam excepturi temporibus
+            libero obcaecati. Tenetur sed velit cum voluptas temporibus ab
+            consequuntur incidunt, culpa nulla laborum perspiciatis! Perferendis
+            itaque natus non aperiam. Debitis, eos sed. Cumque aliquid est
+            commodi rerum repudiandae corrupti eum saepe ex! Sit adipisci
+            ducimus eius esse, velit fugit? Quo iusto deleniti blanditiis neque,
+            esse perspiciatis unde doloribus eligendi fuga minima cum, officia
+            dolores ipsa ad! Quis est sint cumque sed laborum, exercitationem
+            vel ipsam quo quas blanditiis maiores adipisci enim at eius ab
+            maxime quia harum eveniet laudantium? Nesciunt officia aut expedita
+            repellendus hic facere eum est cupiditate, nihil animi rerum. Quam,
+            placeat veniam porro earum expedita illo. Earum perferendis
+            obcaecati eveniet molestias nisi sequi provident nesciunt
+            accusantium similique ullam animi eligendi atque, alias rerum, unde
+            quasi possimus, qui totam voluptatem hic quod reprehenderit?
+            Excepturi omnis, reiciendis quas quia debitis reprehenderit magnam.
+            Alias tempore corrupti, voluptatum dolores perferendis voluptatibus
+            cupiditate libero magni maxime at modi, labore saepe molestiae optio
+            voluptas. Eaque officia doloribus cum non aliquid commodi eius
+            facilis, iure quod quae quisquam aliquam natus placeat ex eveniet
+            qui omnis aspernatur.
+          </div>
+          <div className="product-list-filtered__products">
+            {PRODUCTS_DATA.map((product) => (
+              <WareCard
+                key={product.wareId}
+                className="product-list-filtered__product"
+                wareDate={product}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </section>
--- src/components/ToggleButton/ToggleButton.tsx
+++ src/components/ToggleButton/ToggleButton.tsx
@@ -1,34 +1,27 @@
-import { ButtonHTMLAttributes, useState } from 'react';
+import { ButtonHTMLAttributes } from 'react';
 import './ToggleButton.scss';
 import { SelectIcon } from '../ui/Icons';
 
 type TToggleButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   isActive?: boolean;
-  onClick?: () => void;
   isNotShowIcon?: boolean;
   labelActive: string;
   labelNotActive?: string;
 };
 function ToggleButton({
   className = '',
-  // isActive = false,
-  // onClick = () => {},
+  isActive = false,
   isNotShowIcon = false,
   labelActive,
   labelNotActive,
   ...props
 }: TToggleButtonProps): JSX.Element {
-  const [isActive, setIsActive] = useState(false);
-
   return (
     <button
       {...props}
       className={`toggle-button ${className} ${
         isActive ? 'toggle-button_active' : ''
       } ${!isNotShowIcon ? 'toggle-button_icon' : ''}`}
-      onClick={() => {
-        setIsActive(!isActive);
-      }}
       type="button"
     >
       {!isNotShowIcon && <SelectIcon className="toggle-button__icon" />}
