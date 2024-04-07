7f8598a491bbe80e5f9b954bce7e8a513b1b090d
Alex-Nicalace <anicalace@gmail.com>
2024-04-07 13:21:08 +0300

    RangeSlider: create

---
+++ src/component-library/RangeSlider/RangeSlider.tsx
@@ -0,0 +1,234 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+import './RangeSlider.scss';
+
+type TRangeSliderProps<T> = (
+  | {
+      value: T;
+      defaultValue?: never;
+    }
+  | {
+      value?: never;
+      defaultValue: T;
+    }
+) & {
+  min: number;
+  max: number;
+  step?: number;
+  name?: string;
+  onChange?: (value: T, activeThumb?: number, e?: PointerEvent) => void;
+};
+
+type TValue = number | number[];
+
+function RangeSlider<T extends TValue>({
+  min,
+  max,
+  step = 1,
+  name,
+  defaultValue,
+  value,
+  onChange,
+}: TRangeSliderProps<T>): JSX.Element {
+  const [valueState, setValueState] = useState<number | number[]>(
+    defaultValue ?? 0
+  );
+  const sliderRef = useRef<HTMLDivElement>(null);
+  const trackRef = useRef<HTMLDivElement>(null);
+  const itemsRef = useRef<Map<number, HTMLDivElement>>();
+  const isInnerState = !value;
+  const isNumberState = isInnerState
+    ? typeof valueState === 'number'
+    : typeof value === 'number';
+
+  const getValue = useCallback(
+    function getValue() {
+      const result = (value as TValue) ?? valueState;
+      return Array.isArray(result) ? [...result] : [result];
+    },
+    [value, valueState]
+  );
+
+  function setValue(
+    valueArr: number[],
+    e?: PointerEvent,
+    activeThumb?: number
+  ) {
+    const newValue = (isNumberState ? valueArr[0] : valueArr) as T;
+    if (onChange) {
+      onChange(newValue, activeThumb, e);
+    }
+    if (isInnerState) {
+      setValueState(newValue);
+    }
+  }
+
+  const getMapThumbs = useCallback(function getMapItems() {
+    if (!itemsRef.current) {
+      itemsRef.current = new Map(); // единожды инициализируем Map
+    }
+    return itemsRef.current;
+  }, []);
+
+  const convertPageXToValue = useCallback(
+    function convertPageXToValue(pageX: number, widthSlider: number) {
+      const shiftValue = max - min;
+      const stepValue = (step * widthSlider) / shiftValue;
+      const leftPx = Math.round(pageX / stepValue) * stepValue;
+      const value = Math.round((leftPx / stepValue) * step) + min;
+      return value;
+    },
+    [max, min, step]
+  );
+
+  const convertValueToPx = useCallback(
+    function convertValueToPx(value: number, widthSlider: number) {
+      const shiftValue = max - min;
+      const valueShift = value - min;
+      return (valueShift * widthSlider) / shiftValue;
+    },
+    [max, min]
+  );
+
+  const convertValueToPercent = useCallback(
+    function convertValueToPercent(value: number, widthSlider: number) {
+      const px = convertValueToPx(value, widthSlider);
+      return (px * 100) / widthSlider + '%';
+    },
+    [convertValueToPx]
+  );
+
+  useEffect(
+    function positioningElements() {
+      const widthSlider = sliderRef.current?.offsetWidth ?? 0;
+      const firstValue = convertValueToPx(getValue()[0], widthSlider);
+      const lastValue = convertValueToPx(
+        getValue()[getValue().length - 1],
+        widthSlider
+      );
+      const widthTrack = lastValue - firstValue;
+
+      // изменяем CSS-свойства закрашенная полоса
+      trackRef.current?.style.setProperty(
+        '--left',
+        `${(firstValue * 100) / widthSlider}%`
+      );
+      trackRef.current?.style.setProperty(
+        '--width',
+        `${(widthTrack * 100) / widthSlider}%`
+      );
+
+      getValue().forEach((value, index) => {
+        getMapThumbs()
+          .get(index)
+          ?.style.setProperty(
+            '--left',
+            convertValueToPercent(value, widthSlider)
+          );
+      });
+    },
+    [getValue, getMapThumbs, convertValueToPx, convertValueToPercent]
+  );
+
+  function handleMouseDownThumb(
+    e: React.PointerEvent<HTMLDivElement>,
+    initIndex: number
+  ) {
+    const sliderEl = sliderRef.current;
+    if (!sliderEl) return;
+
+    e.preventDefault(); // предотвратить выделение
+    const pointerId = e.pointerId;
+    sliderEl.classList.add('range-slider_active');
+
+    let shiftX: number;
+    let maxValue: number;
+    let minValue: number;
+    let indexThumb: number;
+
+    const setVariables = (index: number, values: number[]) => {
+      shiftX = 0; // e.clientX - thumbCoord.left - (thumbCoord.width / 2);
+      indexThumb = index;
+      minValue = values[indexThumb - 1];
+      maxValue = values[indexThumb + 1];
+    };
+    setVariables(initIndex, getValue());
+
+    // перенацелить все события указателя (до pointerup) на slider
+    sliderEl.setPointerCapture(pointerId);
+
+    const sliderCoord = sliderEl.getBoundingClientRect();
+    const widthSlider = sliderCoord.width;
+    const minSliderValue = 0;
+
+    const copyValue = getValue();
+    const moveAt = (pageX: number, e?: PointerEvent) => {
+      // передвинуть ползунок под координаты курсора
+      // и сдвинуть на половину ширины/высоты для центрирования
+      const valueX = Math.min(
+        Math.max(pageX - shiftX - sliderCoord.left, minSliderValue),
+        widthSlider
+      );
+      const value = convertPageXToValue(valueX, widthSlider);
+      copyValue[indexThumb] = value;
+
+      if (value >= maxValue || value <= minValue) {
+        const increment = value <= minValue ? -1 : 1;
+        setVariables(indexThumb + increment, copyValue);
+      }
+
+      setValue([...copyValue], e, indexThumb);
+    };
+
+    moveAt(e.pageX);
+    // (3) перемещать по экрану
+    sliderEl.addEventListener('pointermove', onMouseMove);
+    // (4) положить объект, удалить более ненужные обработчики событий
+    const onMouseUp = () => {
+      sliderEl.removeEventListener('pointerup', onMouseUp);
+      sliderEl.removeEventListener('pointermove', onMouseMove);
+
+      sliderEl.classList.remove('range-slider_active');
+    };
+    sliderEl.addEventListener('pointerup', onMouseUp);
+
+    function onMouseMove(e: PointerEvent) {
+      moveAt(e.pageX, e);
+    }
+  }
+
+  return (
+    <div className="range-slider" ref={sliderRef}>
+      <div className="range-slider__rail"></div>
+      <div className="range-slider__track" ref={trackRef}></div>
+      {getValue().map((value, index) => (
+        <div
+          key={index}
+          ref={(node) => {
+            const map = getMapThumbs();
+            if (node) {
+              map.set(index, node);
+            } else {
+              map.delete(index);
+            }
+          }}
+          className="range-slider__thumb"
+          data-index={index}
+          data-value={value}
+          onPointerDown={(e) => handleMouseDownThumb(e, index)}
+          // onMouseDownCapture={handleMouseDownThumb}
+        >
+          <input
+            type="range"
+            name={name}
+            min={min}
+            max={max}
+            step={step}
+            defaultValue={value}
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default RangeSlider;
