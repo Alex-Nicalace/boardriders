16b592ff35566b90bfba19c24f322821766e09c5
Alex-Nicalace <anicalace@gmail.com>
2024-04-26 15:07:50 +0300

    Checkbox: add variants

--- src/components/ui/Checkbox/Checkbox.tsx
+++ src/components/ui/Checkbox/Checkbox.tsx
@@ -1,51 +1,128 @@
 import './Checkbox.scss';
 import { CheckIcon } from '../Icons';
+import React from 'react';
 
-type TCheckboxProps = {
+export type TColor =
+  | {
+      color: string;
+      pathImg?: undefined;
+    }
+  | {
+      color?: undefined;
+      pathImg: string;
+    };
+
+export type TCheckboxProps = (
+  | {
+      variant?: 'checkbox';
+      label?: React.ReactNode;
+      hint?: React.ReactNode;
+    }
+  | ({
+      variant: 'btn-color';
+      label?: never;
+      hint?: never;
+    } & TColor)
+  | {
+      variant: 'btn-toggle';
+      label?: string;
+      hint?: never;
+    }
+) & {
   name?: string;
   id?: string;
-  label?: React.ReactNode;
   className?: string;
   checked?: boolean;
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
   value?: string;
   type?: 'checkbox' | 'radio';
-  hint?: React.ReactNode;
+  disabled?: boolean;
 };
 function Checkbox({
   name,
   id,
-  label,
   className = '',
   checked,
   onChange,
   value,
   type = 'checkbox',
-  hint,
+  disabled,
+  ...props
 }: TCheckboxProps): JSX.Element {
-  const isReactElement = typeof label === 'object';
+  let innerHtml: React.ReactNode;
+  let classes = '';
+  let nameBlock = 'checkbox';
+
+  switch (props.variant) {
+    case 'btn-color':
+      {
+        const { color, pathImg } = props;
+        classes = `${className} checkbox-color`;
+        nameBlock = 'checkbox-color';
+
+        innerHtml = (
+          <span className="checkbox-color__wrap">
+            <span
+              className="checkbox-color__color"
+              style={{ background: color }}
+            >
+              {pathImg && <img src={pathImg} alt="цвет" loading="lazy" />}
+            </span>
+          </span>
+        );
+      }
+      break;
+
+    case 'btn-toggle':
+      {
+        const { label } = props;
+        classes = 'btn-toggle';
+        nameBlock = 'btn-toggle';
+
+        innerHtml = (
+          <>
+            <span className="btn-toggle__inner">{label}</span>
+          </>
+        );
+      }
+      break;
+    default:
+      {
+        const { label, hint } = props;
+        const isReactElement = typeof label === 'object';
+        classes = `checkbox ${type === 'radio' ? 'checkbox_radio' : ''}`;
+
+        innerHtml = (
+          <>
+            <span className="checkbox__icon">
+              {type === 'checkbox' && (
+                <CheckIcon className="checkbox__icon-check" />
+              )}
+            </span>
+            {!isReactElement && (
+              <span className="checkbox__label">{label}</span>
+            )}
+            {isReactElement && label}
+            {hint && <span className="checkbox__hint">{hint}</span>}
+          </>
+        );
+      }
+      break;
+  }
 
   return (
-    <label
-      className={`${className} checkbox ${
-        type === 'radio' ? 'checkbox_radio' : ''
-      }`}
-    >
+    <label className={`${classes} ${className}`}>
       <input
-        className="checkbox__input sr-only"
+        className={`${nameBlock}__input sr-only`}
         type={type}
         name={name}
         id={id}
-        checked={checked}
+        defaultChecked={checked}
         onChange={onChange}
         value={value}
+        disabled={disabled}
       />
-      <span className="checkbox__icon">
-        {type === 'checkbox' && <CheckIcon className="checkbox__icon-check" />}
-      </span>
-      {!isReactElement && <span className="checkbox__label">{label}</span>}
-      {isReactElement && label}
-      {hint && <span className="checkbox__hint">{hint}</span>}
+      {innerHtml}
     </label>
   );
 }
