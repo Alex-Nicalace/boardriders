690b7872f00b14913a7469b9c31b7ed0ea5b8205
Alex-Nicalace <anicalace@gmail.com>
2024-04-30 13:23:14 +0300

    ProductPage: usage SliderWareCards

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -4,6 +4,7 @@ import Product from '../../components/Product';
 import Breadcrumbs, { TBreadcrumbsData } from '../../components/ui/Breadcrumbs';
 import Steps from '../../components/ui/Steps';
 import { useScreenWidth } from '../../Context/useScreenWidthContext';
+import SliderWareCards from '../../components/ui/SliderWareCards';
 
 const BREADCRUMBS: TBreadcrumbsData = [
   {
@@ -24,6 +25,71 @@ const BREADCRUMBS: TBreadcrumbsData = [
   },
   {
     title: 'Сноуборд GNU Asym Carbon Credit Btx Multicolor',
+  },
+];
+
+const PATH_POPULAR = '/src/assets/img/popular-collection/';
+const POPULAR_PRODUCTS_DATA = [
+  {
+    wareId: '1',
+    imgMain: PATH_POPULAR + '01.png',
+    imgSecond: PATH_POPULAR + '01-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    newPrice: 17392,
+    discount: -50,
+    to: '#',
+  },
+  {
+    wareId: '2',
+    imgMain: PATH_POPULAR + '02.png',
+    imgSecond: PATH_POPULAR + '02-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    newPrice: 17392,
+    discount: -50,
+    to: '#',
+  },
+  {
+    wareId: '3',
+    imgMain: PATH_POPULAR + '03.png',
+    imgSecond: PATH_POPULAR + '03-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    to: '#',
+  },
+  {
+    wareId: '4',
+    imgMain: PATH_POPULAR + '04.png',
+    imgSecond: PATH_POPULAR + '04-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    to: '#',
+  },
+  // -----------
+  {
+    wareId: '5',
+    imgMain: PATH_POPULAR + '01.png',
+    imgSecond: PATH_POPULAR + '01-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    to: '#',
+  },
+  {
+    wareId: '6',
+    imgMain: PATH_POPULAR + '02.png',
+    imgSecond: PATH_POPULAR + '02-hover.png',
+    title: 'LIB TECH',
+    descr: 'Мужской Сноуборд',
+    price: 34392,
+    newPrice: 17392,
+    discount: -50,
+    to: '#',
   },
 ];
 
@@ -48,6 +114,24 @@ function ProductPage(/*{ }: TProductPageProps*/): JSX.Element {
       <Main className="product-page__main">
         <Product className="product-page__product" />
       </Main>
+      <section className="product-page__box-slider-ware">
+        <h2 className="product-page__title-slider product-page__container">
+          C этим товаром смотрят
+        </h2>
+        <SliderWareCards
+          className="product-page__slider-ware"
+          wareData={POPULAR_PRODUCTS_DATA}
+        />
+      </section>
+      <section className="product-page__box-slider-ware">
+        <h2 className="product-page__title-slider product-page__container">
+          C этим товаром рекомендуем
+        </h2>
+        <SliderWareCards
+          className="product-page__slider-ware"
+          wareData={POPULAR_PRODUCTS_DATA}
+        />
+      </section>
     </div>
   );
 }
