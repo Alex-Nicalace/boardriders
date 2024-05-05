36092e255ea9952c9b05fdba1d3fefeaeb5f8d2d
Alex-Nicalace <anicalace@gmail.com>
2024-05-05 10:11:53 +0300

    Review: create

---
+++ src/components/Review/Review.tsx
@@ -0,0 +1,45 @@
+import Rating from '../../component-library/Rating';
+import { StarIcon } from '../ui/Icons';
+import './Review.scss';
+type TReviewProps = {
+  rating?: number;
+  name: string;
+  message?: React.ReactNode;
+  dateString: string;
+};
+function Review({
+  rating,
+  name,
+  message,
+  dateString,
+}: TReviewProps): JSX.Element {
+  const formaterDate = new Intl.DateTimeFormat('ru', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+
+  return (
+    <article className="review">
+      <header className="review__header">
+        <h2 className="review__title">{name}</h2>
+        <time className="review__date" dateTime={dateString}>
+          {formaterDate.format(new Date(dateString))}
+        </time>
+        <Rating
+          className="review__rating"
+          rating={rating}
+          iconActiveElement={<StarIcon fill="#EB5757" stroke="#EB5757" />}
+          iconUnactiveElement={<StarIcon fill="transparent" stroke="#bdbdbd" />}
+          size="20px"
+          gap="10px"
+          isShowValue={false}
+          disabled
+        />
+      </header>
+      <p className="review__text">{message}</p>
+    </article>
+  );
+}
+
+export default Review;
