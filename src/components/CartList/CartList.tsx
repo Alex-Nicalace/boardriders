e42bdd70bb2941acb68215d5456d80fd43c0ff62
Alex-Nicalace <anicalace@gmail.com>
2024-05-14 16:57:24 +0300

    CartList: create

---
+++ src/components/CartList/CartList.tsx
@@ -0,0 +1,54 @@
+import './CartList.scss';
+import { useFormaters } from '../../Context/useFormaters';
+import WareCardCart from '../ui/WareCardCart';
+import { useScreenWidth } from '../../Context/useScreenWidthContext';
+
+type TCartListProps = {
+  className?: string;
+  cartData: {
+    title: string;
+    img: string;
+    article: string;
+    props: {
+      name: string;
+      value: string;
+    }[];
+    price: number;
+  }[];
+};
+function CartList({ className, cartData }: TCartListProps): JSX.Element {
+  const { formaterCurrency } = useFormaters();
+  const { isLessPC, isLessTablet, isLessMobile } = useScreenWidth();
+  console.log(isLessPC);
+
+  const totalPrice = cartData.reduce((acc, item) => acc + item.price, 0);
+  const totalItems = cartData.length;
+
+  const classes = ['cart-list', className].filter(Boolean).join(' ');
+
+  return (
+    <div className={classes}>
+      <ul className="cart-list__list">
+        {cartData.map((item) => (
+          <li className="cart-list__item" key={item.article}>
+            <WareCardCart
+              data={item}
+              mode={isLessMobile ? 'mobile' : 'desktop'}
+            />
+          </li>
+        ))}
+      </ul>
+
+      <div className="cart-list__total">
+        <span className="cart-list__total-text">
+          {totalItems} товар(а) на сумму
+        </span>
+        <span className="cart-list__total-value">
+          {formaterCurrency(totalPrice)}
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default CartList;
