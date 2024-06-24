a3f496524a12194fc935be9b09ab67ceb735e7c1
Alex-Nicalace <anicalace@gmail.com>
2024-06-24 17:34:10 +0300

    PersonalInfo: create

---
+++ src/components/PersonalInfo/PersonalInfo.tsx
@@ -0,0 +1,41 @@
+import { useFormaters } from '../../Context/useFormaters';
+import './PersonalInfo.scss';
+
+enum SEX {
+  Женский,
+  Мужской,
+}
+
+type TPersonalInfoProps = {
+  className?: string;
+  fullName: string;
+  sex?: SEX;
+  dateBirth?: Date;
+  phone?: string;
+  email?: string;
+  password: string;
+};
+function PersonalInfo({
+  className,
+  fullName,
+  sex,
+  dateBirth,
+  phone,
+  email,
+  password,
+}: TPersonalInfoProps): JSX.Element {
+  const { formaterDateShort } = useFormaters();
+
+  return (
+    <div className={['personal-info', className].filter(Boolean).join(' ')}>
+      <p>{fullName}</p>
+      {sex && <p>{SEX[sex]}</p>}
+      {dateBirth && <p>{formaterDateShort(dateBirth)}</p>}
+      {phone && <p>{phone}</p>}
+      {email && <p>{email}</p>}
+      <p>Пароль: {'*'.repeat(password.length)}</p>
+    </div>
+  );
+}
+
+export default PersonalInfo;
