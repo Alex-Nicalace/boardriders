36ccc0a2310661958a8ffbdf3cb292038040c113
Alex-Nicalace <anicalace@gmail.com>
2024-06-24 09:20:46 +0300

    AccountMainData: usage OrderStatusList

--- src/components/AccountMainData/AccountMainData.tsx
+++ src/components/AccountMainData/AccountMainData.tsx
@@ -1,13 +1,28 @@
+import OrderStatusList from '../OrderStatusList';
 import ShoppngList from '../ShoppngList';
 import StatCard from '../StatCard';
 import './AccountMainData.scss';
 
 const SHOPPING_LIST = [
-  { name: 'Сноуборд GNU Asym Carbon Credit Btx Multicolor' },
-  { name: 'Утепленные кроссовки DC SHOES E.Tribeka WNT' },
-  { name: 'Шлем DC SHOES E.Tribeka WNT' },
-  { name: 'Ботинки сноубордические DC Shoes' },
-  { name: 'Утепленные кроссовки DC SHOES E.Tribeka' },
+  { name: 'Сноуборд GNU Asym Carbon Credit Btx Multicolor', price: 30 },
+  { name: 'Утепленные кроссовки DC SHOES E.Tribeka WNT', price: 40 },
+  { name: 'Шлем DC SHOES E.Tribeka WNT', price: 50 },
+  { name: 'Ботинки сноубордические DC Shoes', price: 60 },
+  { name: 'Утепленные кроссовки DC SHOES E.Tribeka', price: 70 },
+
+  { name: 'Сноуборд GNU Asym Carbon Credit Btx Multicolor_', price: 30 },
+  { name: 'Утепленные кроссовки DC SHOES E.Tribeka WNT_', price: 40 },
+  { name: 'Шлем DC SHOES E.Tribeka WNT_', price: 50 },
+  { name: 'Ботинки сноубордические DC Shoes_', price: 60 },
+  { name: 'Утепленные кроссовки DC SHOES E.Tribeka_', price: 70 },
+];
+
+const ORDER_STATUS = [
+  { code: '195455-222546-ANU', status: 2, price: 118570, isPaid: true },
+  { code: '295455-222546-ANU', status: 2, price: 45258, isPaid: true },
+  { code: '395455-222546-ANU', status: 0, price: 1544, isPaid: false },
+  { code: '495455-222546-ANU', status: 1, price: 1545, isPaid: false },
+  { code: '595455-222546-ANU', status: 2, price: 45285, isPaid: true },
 ];
 // type TAccountMainDataProps = { }
 function AccountMainData(/*{ }: TAccountMainDataProps*/): JSX.Element {
@@ -19,7 +34,10 @@ function AccountMainData(/*{ }: TAccountMainDataProps*/): JSX.Element {
           hint="10 товаров"
           labelTarget="перейти в корзину"
         >
-          <ShoppngList data={SHOPPING_LIST} />
+          <ShoppngList data={SHOPPING_LIST} limitListCount={5} />
+        </StatCard>
+        <StatCard title="заказы" labelTarget="все заказы">
+          <OrderStatusList data={ORDER_STATUS} />
         </StatCard>
       </div>
     </div>
