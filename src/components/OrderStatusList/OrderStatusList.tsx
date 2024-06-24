87c3755d37f324a5cefe128d9b1a1e12ad80c640
Alex-Nicalace <anicalace@gmail.com>
2024-06-24 17:33:01 +0300

    OrderStatusList: usege useResizeObserver

--- src/components/OrderStatusList/OrderStatusList.tsx
+++ src/components/OrderStatusList/OrderStatusList.tsx
@@ -1,3 +1,5 @@
+import { useMemo, useRef } from 'react';
+import { useResizeObserver } from '../../hooks/useResizeObserver';
 import OrderStatus, { TOrderStatusData } from '../OrderStatus';
 import './OrderStatusList.scss';
 
@@ -6,12 +8,21 @@ type TOrderListProps = {
   data: TOrderStatusData[];
 };
 function OrderStatusList({ className, data }: TOrderListProps): JSX.Element {
+  const ulRef = useRef<HTMLUListElement>(null);
+  const refsMemo = useMemo(() => [ulRef], [ulRef]);
+  const [ulSize] = useResizeObserver(refsMemo);
+  const { width } = ulSize || {};
+
   return (
-    <ul className={['order-status-list', className].filter(Boolean).join(' ')}>
+    <ul
+      ref={ulRef}
+      className={['order-status-list', className].filter(Boolean).join(' ')}
+    >
       {data.map((item) => (
         <li key={item.code}>
           <OrderStatus
-            {...item} // mode="mobile"
+            {...item}
+            mode={width && width < 500 ? 'mobile' : undefined}
           />
         </li>
       ))}
