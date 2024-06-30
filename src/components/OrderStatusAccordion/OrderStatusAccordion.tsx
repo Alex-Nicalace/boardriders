4c98090f2897db81edc1c751ff6d3ef54bc653de
Alex-Nicalace <anicalace@gmail.com>
2024-06-30 10:34:24 +0300

    OrderStatusAccordion: usage OrderStatusDetails

--- src/components/OrderStatusAccordion/OrderStatusAccordion.tsx
+++ src/components/OrderStatusAccordion/OrderStatusAccordion.tsx
@@ -1,6 +1,7 @@
 import { useScreenWidth } from '../../Context/useScreenWidthContext';
 import Accordion from '../../component-library/Accordion';
 import OrderStatus, { TOrderStatusData } from '../OrderStatus';
+import OrderStatusDetails from '../OrderStatusDetails';
 import './OrderStatusAccordion.scss';
 
 type TOrderStatusAccordionProps = {
@@ -37,7 +38,7 @@ function OrderStatusAccordion({
                 </span>
               )}
 
-              {(!isLessMobile || (!open && isLessMobile)) && (
+              {!open && (
                 <OrderStatus
                   className="order-status-accordion__status"
                   {...item}
@@ -46,14 +47,7 @@ function OrderStatusAccordion({
               )}
             </>
           )}
-          contentNode={
-            <>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam iure
-              libero necessitatibus, dignissimos provident qui inventore eius
-              maiores labore illum, aperiam consectetur vel eos et autem quo,
-              nihil nobis reprehenderit?
-            </>
-          }
+          contentNode={<OrderStatusDetails />}
         />
       ))}
     </Accordion>
