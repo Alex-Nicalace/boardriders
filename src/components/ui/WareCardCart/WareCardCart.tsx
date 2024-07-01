6c4f0359e56fa48c7391a81daac9f297ba97a57e
Alex-Nicalace <anicalace@gmail.com>
2024-07-01 15:00:25 +0300

    WareCardCart: add prop isOrdered, prop data extend prop count

--- src/components/ui/WareCardCart/WareCardCart.tsx
+++ src/components/ui/WareCardCart/WareCardCart.tsx
@@ -7,24 +7,28 @@ import { useFormaters } from '../../../Context/useFormaters';
 type TWareCardCartProps = {
   className?: string;
   mode?: 'desktop' | 'mobile';
+  isOrdered?: boolean;
   data: {
     title: string;
     img: string;
     article: string;
     props: { name: string; value: string }[];
     price: number;
+    count?: number;
   };
 };
 function WareCardCart({
   className = '',
   mode = 'desktop',
-  data: { title, article, img, props, price },
+  data: { title, article, img, props, price, count = 1 },
+  isOrdered = false,
 }: TWareCardCartProps): JSX.Element {
   const { formaterCurrency } = useFormaters();
   const classes = [
     'ware-card-cart',
-    className,
     mode === 'mobile' && 'ware-card-cart_mobile',
+    isOrdered && 'ware-card-cart_ordered',
+    className,
   ]
     .filter(Boolean)
     .join(' ');
@@ -49,11 +53,27 @@ function WareCardCart({
           </li>
         ))}
       </ul>
-      <InputNumber className="ware-card-cart__input" min={1} max={99} />
-      <div className="ware-card-cart__price">{formaterCurrency(price)}</div>
-      <button className="ware-card-cart__btn-delete">
-        <CloseIcon />
-      </button>
+      {!isOrdered && (
+        <InputNumber
+          className="ware-card-cart__count"
+          min={1}
+          max={99}
+          defaultValue={count}
+        />
+      )}
+      {isOrdered && (
+        <div className="ware-card-cart__count">
+          {count} x {formaterCurrency(price)}
+        </div>
+      )}
+      <div className="ware-card-cart__price">
+        {formaterCurrency(isOrdered ? price * count : price)}
+      </div>
+      {!isOrdered && (
+        <button className="ware-card-cart__btn-delete">
+          <CloseIcon />
+        </button>
+      )}
     </article>
   );
 }
