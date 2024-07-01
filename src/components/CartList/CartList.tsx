91788c878089b649f2c463e98e8bb5268c4433d1
Alex-Nicalace <anicalace@gmail.com>
2024-07-01 15:04:36 +0300

    CartList: add prop isOrdered

--- src/components/CartList/CartList.tsx
+++ src/components/CartList/CartList.tsx
@@ -5,6 +5,7 @@ import { useScreenWidth } from '../../Context/useScreenWidthContext';
 
 type TCartListProps = {
   className?: string;
+  isOrdered?: boolean;
   cartData: {
     title: string;
     img: string;
@@ -16,36 +17,54 @@ type TCartListProps = {
     price: number;
   }[];
 };
-function CartList({ className, cartData }: TCartListProps): JSX.Element {
+function CartList({
+  className,
+  cartData,
+  isOrdered = false,
+}: TCartListProps): JSX.Element {
   const { formaterCurrency } = useFormaters();
   const { isLessMobile } = useScreenWidth();
 
   const totalPrice = cartData.reduce((acc, item) => acc + item.price, 0);
   const totalItems = cartData.length;
 
-  const classes = ['cart-list', className].filter(Boolean).join(' ');
+  const classes = ['cart-list', isOrdered && 'cart-list_ordered', className]
+    .filter(Boolean)
+    .join(' ');
 
   return (
     <div className={classes}>
+      {isOrdered && !isLessMobile && (
+        <div className="cart-list__header">
+          <span>Товар</span>
+          <span></span>
+          <span>Цена за единицу</span>
+          <span>Сумма</span>
+        </div>
+      )}
+
       <ul className="cart-list__list">
         {cartData.map((item) => (
           <li className="cart-list__item" key={item.article}>
             <WareCardCart
               data={item}
               mode={isLessMobile ? 'mobile' : 'desktop'}
+              isOrdered={isOrdered}
             />
           </li>
         ))}
       </ul>
 
-      <div className="cart-list__total">
-        <span className="cart-list__total-text">
-          {totalItems} товар(а) на сумму
-        </span>
-        <span className="cart-list__total-value">
-          {formaterCurrency(totalPrice)}
-        </span>
-      </div>
+      {!isOrdered && (
+        <div className="cart-list__total">
+          <span className="cart-list__total-text">
+            {totalItems} товар(а) на сумму
+          </span>
+          <span className="cart-list__total-value">
+            {formaterCurrency(totalPrice)}
+          </span>
+        </div>
+      )}
     </div>
   );
 }
