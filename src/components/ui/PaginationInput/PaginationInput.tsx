dbc7ab85c12e1a2fb98058c0097169eab9d57fd1
Alex-Nicalace <anicalace@gmail.com>
2024-07-02 16:04:12 +0300

    PaginationInput: add props

--- src/components/ui/PaginationInput/PaginationInput.tsx
+++ src/components/ui/PaginationInput/PaginationInput.tsx
@@ -1,12 +1,25 @@
+import { SelectIcon } from '../Icons';
 import './PaginationInput.scss';
 
 type TPaginationInputProps = {
   className?: string;
   hideSizeBox?: boolean;
+  totalItems: number;
+  itemsPerPage: number;
+  onPerPageChange?: (count: number) => void;
+  currentPage: number;
+  totalPages: number;
+  onPageChange?: (page: number) => void;
 };
 function PaginationInput({
   className,
   hideSizeBox = false,
+  totalItems,
+  itemsPerPage,
+  onPerPageChange = () => {},
+  currentPage,
+  totalPages,
+  onPageChange = () => {},
 }: TPaginationInputProps): JSX.Element {
   return (
     <div className={['pagination-input', className].filter(Boolean).join(' ')}>
@@ -14,8 +27,12 @@ function PaginationInput({
         <div className="pagination-input__size-box">
           <span className="pagination-input__text">Показать</span>
           <div className="pagination-input__input-box">
-            <input className="pagination-input__input" />
-            <span className="pagination-input__text">из 23</span>
+            <input
+              className="pagination-input__input"
+              value={itemsPerPage}
+              onChange={(e) => onPerPageChange(Number(e.target.value))}
+            />
+            <span className="pagination-input__text">из {totalItems}</span>
           </div>
         </div>
       )}
@@ -23,12 +40,28 @@ function PaginationInput({
       <div className="pagination-input__page-box">
         <span className="pagination-input__text">Страница</span>
         <div className="pagination-input__page">
-          <button className="pagination-input__btn">{'<'}</button>
+          <button
+            className="pagination-input__btn pagination-input__btn_prev"
+            onClick={() => onPageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            <SelectIcon />
+          </button>
           <div className="pagination-input__input-box">
-            <input className="pagination-input__input" />
-            <span className="pagination-input__text">из 2</span>
+            <input
+              className="pagination-input__input"
+              value={currentPage}
+              onChange={(e) => onPageChange(Number(e.target.value))}
+            />
+            <span className="pagination-input__text">из {totalPages}</span>
           </div>
-          <button className="pagination-input__btn">{'>'}</button>
+          <button
+            className="pagination-input__btn pagination-input__btn_next"
+            onClick={() => onPageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
+            <SelectIcon />
+          </button>
         </div>
       </div>
     </div>
