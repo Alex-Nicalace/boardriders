188da2a46a346038a9f8e6522427b7420e665aaa
Alex-Nicalace <anicalace@gmail.com>
2024-07-08 12:17:42 +0300

    InputPasword: add prop defaultPasswordShow

--- src/components/ui/InputPasword/InputPasword.tsx
+++ src/components/ui/InputPasword/InputPasword.tsx
@@ -1,11 +1,19 @@
 import { InputHTMLAttributes, useState } from 'react';
 import { NotVisibleIcon, VisibleIcon } from '../Icons';
-import InputStyled from '../InputStyled';
+import InputStyled, { TInputStyledcommonProps } from '../InputStyled';
 import './InputPasword.scss';
+import { TInputTextCommonProps } from '../../../component-library/InputText';
 
-type TInputPaswordProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'type'>;
-function InputPasword({ ...props }: TInputPaswordProps): JSX.Element {
-  const [showPassword, setShowPassword] = useState(false);
+type TInputPaswordProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'type'> &
+  TInputTextCommonProps &
+  TInputStyledcommonProps & {
+    defaultPasswordShow?: boolean;
+  };
+function InputPasword({
+  defaultPasswordShow = false,
+  ...props
+}: TInputPaswordProps): JSX.Element {
+  const [showPassword, setShowPassword] = useState(defaultPasswordShow);
 
   return (
     <InputStyled
