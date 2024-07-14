9e0045dcb852a50253acd18e355aed92102b109e
Alex-Nicalace <anicalace@gmail.com>
2024-07-14 16:06:32 +0300

    TabsBlock: add prop variant

--- src/components/TabsBlock/TabsBlock.tsx
+++ src/components/TabsBlock/TabsBlock.tsx
@@ -5,27 +5,43 @@ import { useScreenWidth } from '../../Context/useScreenWidthContext';
 type TTabsBlockProps = Pick<
   React.ComponentProps<typeof Tabs>,
   'className' | 'value' | 'onChange' | 'children'
->;
-function TabsBlock({ className, ...props }: TTabsBlockProps): JSX.Element {
+> & {
+  variant?: 'main' | 'second';
+};
+function TabsBlock({
+  className,
+  variant = 'main',
+  ...props
+}: TTabsBlockProps): JSX.Element {
   const { isLessMobile } = useScreenWidth();
 
   return (
-    <div
-      className={[
-        'tabs-block',
-        !isLessMobile && 'tabs-block__container',
-        className,
-      ]
-        .filter(Boolean)
-        .join(' ')}
-    >
-      <Tabs
-        {...props}
-        variant="scrollable"
-        scrollButtons={!isLessMobile ? 'auto' : false}
-        isDragScrollableTabs
-      />
-    </div>
+    <>
+      {variant === 'main' && (
+        <div
+          className={[
+            `tabs-${variant}`,
+            !isLessMobile && `tabs-${variant}__container`,
+            className,
+          ]
+            .filter(Boolean)
+            .join(' ')}
+        >
+          <Tabs
+            {...props}
+            variant="scrollable"
+            scrollButtons={!isLessMobile ? 'auto' : false}
+            isDragScrollableTabs
+          />
+        </div>
+      )}
+      {variant === 'second' && (
+        <Tabs
+          {...props}
+          className={[`tabs-${variant}`, className].filter(Boolean).join(' ')}
+        />
+      )}
+    </>
   );
 }
 
