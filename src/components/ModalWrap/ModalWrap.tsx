c257732ec876a7a494fa0da7eb932c6c66a5c5e3
Alex-Nicalace <anicalace@gmail.com>
2024-07-14 16:00:26 +0300

    ModalWrap: add prop withDecorFrame

--- src/components/ModalWrap/ModalWrap.tsx
+++ src/components/ModalWrap/ModalWrap.tsx
@@ -4,14 +4,23 @@ import './ModalWrap.scss';
 type TModalWrapProps = {
   children?: React.ReactNode;
   close: () => void;
+  withDecorFrame?: boolean;
 };
-function ModalWrap({ children, close }: TModalWrapProps): JSX.Element {
+function ModalWrap({
+  children,
+  close,
+  withDecorFrame,
+}: TModalWrapProps): JSX.Element {
   return (
-    <div className="modal-wrap">
+    <div
+      className={['modal-wrap', withDecorFrame && 'modal-wrap_framed']
+        .filter(Boolean)
+        .join(' ')}
+    >
       <div className="modal-wrap__content">
         {children}
         <button className="modal-wrap__close-btn" onClick={close}>
-          <CloseIcon />
+          <CloseIcon className="modal-wrap__close-icon" />
         </button>
       </div>
     </div>
