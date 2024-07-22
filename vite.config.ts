32a410e89978f3809b394262ede4d4a5c2d8f6d4
Alex-Nicalace <anicalace@gmail.com>
2024-07-22 14:23:56 +0300

    react-query: install

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,15 @@ import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 import svgr from 'vite-plugin-svgr';
 import checker from 'vite-plugin-checker';
+import pluginQuery from '@tanstack/eslint-plugin-query';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint(), svgr(), checker({ typescript: true })],
+  plugins: [
+    pluginQuery.configs['flat/recommended'],
+    react(),
+    eslint(),
+    svgr(),
+    checker({ typescript: true }),
+  ],
 });
