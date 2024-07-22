560e4a846c76f7919551f9a058e3066dec3b0fb6
Alex-Nicalace <anicalace@gmail.com>
2024-07-22 15:00:35 +0300

    PopularProductsSlider: create

---
+++ src/features/products/PopularProductsSlider.tsx
@@ -0,0 +1,22 @@
+import Empty from '../../components/Empty';
+import Spinner from '../../components/Spinner';
+import SliderWareCards from '../../components/ui/SliderWareCards';
+import { usePopularProducts } from './usePopularProducts';
+
+type TNewProductsSliderProps = {
+  className?: string;
+};
+function PopularProductsSlider({
+  className,
+}: TNewProductsSliderProps): JSX.Element {
+  const { popularProducts, isLoading } = usePopularProducts();
+
+  if (isLoading) return <Spinner />;
+
+  if (!popularProducts || !popularProducts.length)
+    return <Empty resource="Топ продаж" />;
+
+  return <SliderWareCards className={className} data={popularProducts} />;
+}
+
+export default PopularProductsSlider;
