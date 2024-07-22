d0b77b8cdd6f2685333959b90b2a5e59524bef92
Alex-Nicalace <anicalace@gmail.com>
2024-07-22 15:01:57 +0300

    MainPage: usage NewProductsSlider, PopularProductsSlider

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,142 +1,15 @@
+import './MainPage.scss';
 import DualBanners from '../../components/DualBaners';
 import LinksBrands from '../../components/LinksBrands';
 import MainSlider from '../../components/MainSlider';
 import Promotion from '../../components/Promotion';
 import BlogsSlider from '../../components/BlogsSlider';
 import JoinAndSubscrube from '../../components/JoinAndSubscrube/JoinAndSubscrube';
-import './MainPage.scss';
-import SliderWareCards from '../../components/ui/SliderWareCards';
 import Title from '../../components/ui/Title';
 import Button from '../../components/ui/Button';
 import PageContent from '../../components/PageContent';
-
-const PATH_NEW = '/src/assets/img/products-new/';
-const NEW_PRODUCTS_DATA = [
-  {
-    wareId: '1',
-    imgMain: PATH_NEW + '01.png',
-    imgSecond: PATH_NEW + '01-hover.png',
-    title: 'LIB TECH',
-    descr: 'Мужской Сноуборд',
-    price: 34392,
-    to: '#',
-  },
-  {
-    wareId: '2',
-    imgMain: PATH_NEW + '02.png',
-    imgSecond: PATH_NEW + '02-hover.png',
-    title: 'LIB TECH',
-    descr: 'Мужской Сноуборд',
-    price: 34392,
-    newPrice: 17392,
-    discount: -50,
-    to: '#',
-  },
-  {
-    wareId: '3',
-    imgMain: PATH_NEW + '03.png',
-    imgSecond: PATH_NEW + '03-hover.png',
-    title: 'LIB TECH',
-    descr: 'Мужской Сноуборд',
-    price: 34392,
-    to: '#',
-  },
-  {
-    wareId: '4',
-    imgMain: PATH_NEW + '04.png',
-    imgSecond: PATH_NEW + '04-hover.png',
-    title: 'LIB TECH',
-    descr: 'Мужской Сноуборд',
-    price: 34392,
-    to: '#',
-  },
-  // -----------
-  {
-    wareId: '5',
-    imgMain: PATH_NEW + '01.png',
-    imgSecond: PATH_NEW + '01-hover.png',
-    title: 'LIB TECH',
-    descr: 'Мужской Сноуборд',
-    price: 34392,
-    to: '#',
-  },
-  {
-    wareId: '6',
-    imgMain: PATH_NEW + '02.png',
-    imgSecond: PATH_NEW + '02-hover.png',
-    title: 'LIB TECH',
-    descr: 'Мужской Сноуборд',
-    price: 34392,
-    newPrice: 17392,
-    discount: -50,
-    to: '#',
-  },
-];
-
-const PATH_POPULAR = '/src/assets/img/popular-collection/';
-const POPULAR_PRODUCTS_DATA = [
-  {
-    wareId: '1',
-    imgMain: PATH_POPULAR + '01.png',
-    imgSecond: PATH_POPULAR + '01-hover.png',
-    title: 'LIB TECH',
-    descr: 'Мужской Сноуборд',
-    price: 34392,
-    newPrice: 17392,
-    discount: -50,
-    to: '#',
-  },
-  {
-    wareId: '2',
-    imgMain: PATH_POPULAR + '02.png',
-    imgSecond: PATH_POPULAR + '02-hover.png',
-    title: 'LIB TECH',
-    descr: 'Мужской Сноуборд',
-    price: 34392,
-    newPrice: 17392,
-    discount: -50,
-    to: '#',
-  },
-  {
-    wareId: '3',
-    imgMain: PATH_POPULAR + '03.png',
-    imgSecond: PATH_POPULAR + '03-hover.png',
-    title: 'LIB TECH',
-    descr: 'Мужской Сноуборд',
-    price: 34392,
-    to: '#',
-  },
-  {
-    wareId: '4',
-    imgMain: PATH_POPULAR + '04.png',
-    imgSecond: PATH_POPULAR + '04-hover.png',
-    title: 'LIB TECH',
-    descr: 'Мужской Сноуборд',
-    price: 34392,
-    to: '#',
-  },
-  // -----------
-  {
-    wareId: '5',
-    imgMain: PATH_POPULAR + '01.png',
-    imgSecond: PATH_POPULAR + '01-hover.png',
-    title: 'LIB TECH',
-    descr: 'Мужской Сноуборд',
-    price: 34392,
-    to: '#',
-  },
-  {
-    wareId: '6',
-    imgMain: PATH_POPULAR + '02.png',
-    imgSecond: PATH_POPULAR + '02-hover.png',
-    title: 'LIB TECH',
-    descr: 'Мужской Сноуборд',
-    price: 34392,
-    newPrice: 17392,
-    discount: -50,
-    to: '#',
-  },
-];
+import NewProductsSlider from '../../features/products/NewProductsSlider';
+import PopularProductsSlider from '../../features/products/PopularProductsSlider';
 
 // interface IMainPageProps {}
 function MainPage(): JSX.Element {
@@ -153,10 +26,7 @@ function MainPage(): JSX.Element {
         >
           Новинки
         </Title>
-        <SliderWareCards
-          className="main-page__slider-ware"
-          wareData={NEW_PRODUCTS_DATA}
-        />
+        <NewProductsSlider className="main-page__slider-ware" />
         <div className="main-page__container main-page__button-more">
           <Button to="#">Показать больше</Button>
         </div>
@@ -185,12 +55,9 @@ function MainPage(): JSX.Element {
           kind="h1-32-h2-21"
           as="h2"
         >
-          DC Shoes популярное в коллекции
+          Топ продаж
         </Title>
-        <SliderWareCards
-          className="main-page__slider-ware"
-          wareData={POPULAR_PRODUCTS_DATA}
-        />
+        <PopularProductsSlider className="main-page__slider-ware" />
         <div className="main-page__container main-page__button-more">
           <Button to="#">Показать больше</Button>
         </div>
