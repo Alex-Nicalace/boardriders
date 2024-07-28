7e12cd79a1bc119232f3cbebe583261ca157d65a
Alex-Nicalace <anicalace@gmail.com>
2024-07-28 12:07:52 +0300

    ColorChoise: add props value & onChange

--- src/components/ColorChoise/ColorChoise.tsx
+++ src/components/ColorChoise/ColorChoise.tsx
@@ -3,33 +3,38 @@ import Checkbox, { TColor } from '../ui/Checkbox';
 
 type TColorChoiseProps = {
   name: string;
-  items: (TColor & { value: string })[];
+  items: (TColor & { value: string; isEmpty?: boolean; disabled?: boolean })[];
   className?: string;
-  type?: 'checkbox' | 'radio';
+  value?: string | null;
+  onChange?: (value: string) => void;
 };
 function ColorChoise({
   name,
   items,
-  className = '',
-  type = 'radio',
+  className,
+  value,
+  onChange = () => {},
 }: TColorChoiseProps): JSX.Element {
   return (
-    <div className={`color-choise ${className}`}>
+    <div className={['color-choise', className].filter(Boolean).join(' ')}>
       <div className="color-choise__color">
         <span className="color-choise__title">Цвет:</span>
         <span className="color-choise__text">зеленый</span>
       </div>
       <ul className="color-choise__list">
-        {items.map((item) => (
-          <li className="color-choise__item" key={item.value}>
-            {((item.pathImg && !item.color) ||
-              (!item.pathImg && item.color)) && (
+        {items.map(({ isEmpty, ...rest }) => (
+          <li className="color-choise__item" key={rest.value}>
+            {((rest.pathImg && !rest.color) ||
+              (!rest.pathImg && rest.color)) && (
               <Checkbox
                 className="color-choise__checkbox"
                 variant="btn-color"
-                {...item}
+                {...rest}
                 name={name}
-                type={type}
+                type="radio"
+                onChange={() => onChange(rest.value)}
+                checked={value !== undefined ? rest.value === value : undefined}
+                styleDisabled={isEmpty}
               />
             )}
           </li>
