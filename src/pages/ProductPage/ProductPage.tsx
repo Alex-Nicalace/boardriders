5219c2dcb2349414b78a7b4ee32e011b4498e71a
Alex-Nicalace <anicalace@gmail.com>
2024-07-28 12:19:27 +0300

    ProductPage: usage ProductContainer

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -1,11 +1,11 @@
 import './ProductPage.scss';
-import Product from '../../components/Product';
 import Breadcrumbs, { TBreadcrumbsData } from '../../components/ui/Breadcrumbs';
 import Steps from '../../components/ui/Steps';
 import { useScreenWidth } from '../../Context/useScreenWidthContext';
-import SliderWareCards from '../../components/ui/SliderWareCards';
 import CustomerReviews from '../../components/CustomerReviews';
 import PageContent from '../../components/PageContent';
+import PopularProductsSlider from '../../features/products/PopularProductsSlider';
+import ProductContainer from '../../features/products/ProductContainer';
 
 const BREADCRUMBS: TBreadcrumbsData = [
   {
@@ -29,71 +29,6 @@ const BREADCRUMBS: TBreadcrumbsData = [
   },
 ];
 
-const PATH_POPULAR = '/src/assets/img/popular-collection/';
-const POPULAR_PRODUCTS_DATA = [
-  {
-    id: 1,
-    imgMainUrl: PATH_POPULAR + '01.png',
-    imgSecondUrl: PATH_POPULAR + '01-hover.png',
-    name: 'LIB TECH',
-    description: 'Мужской Сноуборд',
-    price: 17392,
-    oldPrice: 34392,
-    discount: -50,
-  },
-  {
-    id: 2,
-    imgMainUrl: PATH_POPULAR + '02.png',
-    imgSecondUrl: PATH_POPULAR + '02-hover.png',
-    name: 'LIB TECH',
-    description: 'Мужской Сноуборд',
-    price: 17392,
-    oldPrice: 34392,
-    discount: -50,
-  },
-  {
-    id: 3,
-    imgMainUrl: PATH_POPULAR + '03.png',
-    imgSecondUrl: PATH_POPULAR + '03-hover.png',
-    name: 'LIB TECH',
-    description: 'Мужской Сноуборд',
-    price: 34392,
-    oldPrice: null,
-    discount: null,
-  },
-  {
-    id: 4,
-    imgMainUrl: PATH_POPULAR + '04.png',
-    imgSecondUrl: PATH_POPULAR + '04-hover.png',
-    name: 'LIB TECH',
-    description: 'Мужской Сноуборд',
-    price: 34392,
-    oldPrice: null,
-    discount: null,
-  },
-  // -----------
-  {
-    id: 5,
-    imgMainUrl: PATH_POPULAR + '01.png',
-    imgSecondUrl: PATH_POPULAR + '01-hover.png',
-    name: 'LIB TECH',
-    description: 'Мужской Сноуборд',
-    price: 34392,
-    oldPrice: null,
-    discount: null,
-  },
-  {
-    id: 6,
-    imgMainUrl: PATH_POPULAR + '02.png',
-    imgSecondUrl: PATH_POPULAR + '02-hover.png',
-    name: 'LIB TECH',
-    description: 'Мужской Сноуборд',
-    price: 17392,
-    oldPrice: 34392,
-    discount: -50,
-  },
-];
-
 // type TProductPageProps = { }
 function ProductPage(/*{ }: TProductPageProps*/): JSX.Element {
   const { isLessTablet } = useScreenWidth();
@@ -113,25 +48,21 @@ function ProductPage(/*{ }: TProductPageProps*/): JSX.Element {
         </div>
       )}
       <main className="product-page__main">
-        <Product className="product-page__product" />
+        <ProductContainer className="product-page__product" />
       </main>
       <section className="product-page__box-slider-ware">
         <h2 className="product-page__title-slider product-page__container">
           C этим товаром смотрят
         </h2>
-        <SliderWareCards
-          className="product-page__slider-ware"
-          data={POPULAR_PRODUCTS_DATA}
-        />
+        {/* //! для соответствия макету добавил популярные продукты, а не "C этим товаром смотрят" т.к. в БД нет функционала*/}
+        <PopularProductsSlider className="product-page__slider-ware" />
       </section>
       <section className="product-page__box-slider-ware">
         <h2 className="product-page__title-slider product-page__container">
           C этим товаром рекомендуем
         </h2>
-        <SliderWareCards
-          className="product-page__slider-ware"
-          data={POPULAR_PRODUCTS_DATA}
-        />
+        {/* //! для соответствия макету добавил популярные продукты, а не "C этим товаром рекомендуем" т.к. в БД нет функционала*/}
+        <PopularProductsSlider className="product-page__slider-ware" />
       </section>
       <CustomerReviews className="product-page__customer-reviews" />
     </PageContent>
