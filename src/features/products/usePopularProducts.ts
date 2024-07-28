4996ca6f0d702b955397284562e49e7fd0c5b2e5
Alex-Nicalace <anicalace@gmail.com>
2024-07-28 12:16:21 +0300

    usePopularProducts: add arg "limit"

--- src/features/products/usePopularProducts.ts
+++ src/features/products/usePopularProducts.ts
@@ -1,14 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import { getPopularProducts } from '../../services/apiProducts';
 
-export function usePopularProducts() {
+export function usePopularProducts(limit: number) {
   const {
     data: popularProducts,
     isLoading,
     error,
   } = useQuery({
     queryKey: ['topSaleProducts'],
-    queryFn: getPopularProducts,
+    queryFn: () => getPopularProducts(limit),
   });
 
   return { popularProducts, isLoading, error };
