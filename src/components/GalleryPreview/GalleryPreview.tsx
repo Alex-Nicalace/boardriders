0d9d96485de0564b2472e950b8b5f83d7527ff11
Alex-Nicalace <anicalace@gmail.com>
2024-07-29 16:51:44 +0300

    GalleryPreview: add prop "data"

--- src/components/GalleryPreview/GalleryPreview.tsx
+++ src/components/GalleryPreview/GalleryPreview.tsx
@@ -14,37 +14,13 @@ import './GalleryPreview.scss';
 import 'swiper/scss/scrollbar';
 import 'swiper/scss/thumbs';
 import 'swiper/scss/pagination';
+import { ImageSizes } from '../../utils/types';
 
-const PATH_BIG_IMG = './src/assets/img/gallery-preview/big-img/';
-const PATH_SMALL_IMG = './src/assets/img/gallery-preview/small-img/';
-
-const GALLARY_BIG = [
-  '01.webp',
-  '02.webp',
-  '03.webp',
-  '04.webp',
-  '05.webp',
-  '06.webp',
-  '07.webp',
-  '08.webp',
-  '09.webp',
-  '10.webp',
-].map((name) => PATH_BIG_IMG + name);
-const GALLARY_SMALL = [
-  '01.jpg',
-  '02.jpg',
-  '03.jpg',
-  '04.jpg',
-  '05.jpg',
-  '06.jpg',
-  '07.jpg',
-  '08.jpg',
-  '09.jpg',
-  '10.jpg',
-].map((name) => PATH_SMALL_IMG + name);
-
-type TGalleryPreviewProps = { className?: string };
-function GalleryPreview({ className = '' }: TGalleryPreviewProps): JSX.Element {
+type TGalleryPreviewProps = { className?: string; data: string[] };
+function GalleryPreview({
+  className,
+  data,
+}: TGalleryPreviewProps): JSX.Element {
   const [thumbsSwiper, setThumbsSwiper] = useState<SwiperClass | null>(null);
   const { isLessTablet } = useScreenWidth();
 
@@ -64,12 +40,12 @@ function GalleryPreview({ className = '' }: TGalleryPreviewProps): JSX.Element {
           scrollbar
           mousewheel={true} // прокрутка колесом мыши, модуль Mousewheel
         >
-          {GALLARY_SMALL.map((img) => (
-            <SwiperSlide key={img} className="gallery-preview__thumbs-slide">
+          {data.map((url) => (
+            <SwiperSlide key={url} className="gallery-preview__thumbs-slide">
               <img
                 className="gallery-preview__thumbs-img gallery-preview__img"
-                src={img}
-                alt=""
+                src={`${url}${ImageSizes.Thumbnail}`}
+                alt="Фото эскиза товара"
               />
             </SwiperSlide>
           ))}
@@ -100,12 +76,12 @@ function GalleryPreview({ className = '' }: TGalleryPreviewProps): JSX.Element {
           },
         }}
       >
-        {GALLARY_BIG.map((img) => (
-          <SwiperSlide key={img} className="gallery-preview__main-slide">
+        {data.map((url) => (
+          <SwiperSlide key={url} className="gallery-preview__main-slide">
             <img
               className="gallery-preview__main-img gallery-preview__img"
-              src={img}
-              alt=""
+              src={`${url}${ImageSizes.ActiveGallary}`}
+              alt="Фото галереи"
             />
           </SwiperSlide>
         ))}
