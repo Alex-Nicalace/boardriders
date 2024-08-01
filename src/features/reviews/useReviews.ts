8d0fe470bdafe8f44f57b1612d54aacdf2104df2
Alex-Nicalace <anicalace@gmail.com>
2024-08-01 15:46:48 +0300

    useReviews: preloading pages

--- src/features/reviews/useReviews.ts
+++ src/features/reviews/useReviews.ts
@@ -1,22 +1,42 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { useParams, useSearchParams } from 'react-router-dom';
 import { getReviews } from '../../services/apiReviews';
+import { PAGE_SIZE_COMMENTS } from '../../services/constants';
 
 export function useReviews() {
   const params = useParams();
   const [searchParams] = useSearchParams();
+  const queryClient = useQueryClient();
 
   const productId = Number(params.productId);
-  const page = Number(searchParams.get('pageReviews') || 1);
+  const page = Number(searchParams.get('pageReviews'));
+  const pageNum = isNaN(page) ? 1 : page;
 
   const {
-    data: { reviews, count, totalPage } = {},
+    data: { reviews, count } = {},
     isLoading,
     error,
   } = useQuery({
-    queryKey: ['reviews', productId, page],
-    queryFn: () => getReviews({ productId, page }),
+    queryKey: ['reviews', productId, pageNum],
+    queryFn: () => getReviews({ productId, page: pageNum }),
   });
 
-  return { reviews, isLoading, error, count, totalPage };
+  const totalPage = Math.ceil((count ?? 0) / PAGE_SIZE_COMMENTS);
+
+  // предварительная подгрузка следующей страницы
+  if (pageNum < totalPage) {
+    queryClient.prefetchQuery({
+      queryKey: ['reviews', productId, pageNum + 1],
+      queryFn: () => getReviews({ productId, page: pageNum + 1 }),
+    });
+  }
+  // предварительная подгрузка предыдущей страницы
+  if (pageNum > 1) {
+    queryClient.prefetchQuery({
+      queryKey: ['reviews', productId, pageNum - 1],
+      queryFn: () => getReviews({ productId, page: pageNum - 1 }),
+    });
+  }
+
+  return { reviews, isLoading, error, count, totalPage, pageNum };
 }
