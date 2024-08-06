d9b28ea589a3f1db11c1b2c35306313cbd3d6186
Alex-Nicalace <anicalace@gmail.com>
2024-08-06 15:17:51 +0300

    useProduct: add arg "isGetFromCache"

--- src/features/products/useProduct.ts
+++ src/features/products/useProduct.ts
@@ -2,7 +2,13 @@ import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 import { getProduct } from '../../services/apiProducts';
 
-export function useProduct() {
+export function useProduct({
+  isGetFromCache = false,
+}:
+  | {
+      isGetFromCache?: boolean;
+    }
+  | undefined = {}) {
   const params = useParams();
   const productId = Number(params.productId);
 
@@ -13,6 +19,7 @@ export function useProduct() {
   } = useQuery({
     queryKey: ['product', productId],
     queryFn: () => getProduct(productId),
+    enabled: !isGetFromCache,
   });
 
   return { product, isLoading, error };
