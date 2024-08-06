85bc4818425990e13f5a31128f02f22a2d5a9441
Alex-Nicalace <anicalace@gmail.com>
2024-08-06 15:42:31 +0300

    useProducts: create

---
+++ src/features/products/useProducts.ts
@@ -0,0 +1,57 @@
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useParams, useSearchParams } from 'react-router-dom';
+import { getProducts } from '../../services/apiProducts';
+import { PAGE_SIZE_PRODUCTS } from '../../services/constants';
+
+export function useProducts() {
+  const params = useParams();
+  const [searchParams] = useSearchParams();
+  const queryClient = useQueryClient();
+
+  const { categoryGender, categoryMenu, categorySubmenu } = params;
+  const categories = [categoryGender, categoryMenu, categorySubmenu].filter(
+    (item) => item !== undefined
+  );
+  const page = Number(searchParams.get('page'));
+  const pageNum = isNaN(page) || page < 1 ? 1 : page;
+
+  const {
+    data: { products, count } = {},
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ['products', ...categories, pageNum],
+    queryFn: () =>
+      getProducts({
+        categories,
+        page: pageNum,
+      }),
+  });
+
+  const totalPages = Math.ceil((count ?? 0) / PAGE_SIZE_PRODUCTS);
+
+  // предварительная подгрузка следующей страницы
+  if (pageNum < totalPages) {
+    queryClient.prefetchQuery({
+      queryKey: ['products', ...categories, pageNum + 1],
+      queryFn: () =>
+        getProducts({
+          categories,
+          page: pageNum + 1,
+        }),
+    });
+  }
+  // предварительная подгрузка предыдущей страницы
+  if (pageNum > 1) {
+    queryClient.prefetchQuery({
+      queryKey: ['products', ...categories, pageNum - 1],
+      queryFn: () =>
+        getProducts({
+          categories,
+          page: pageNum - 1,
+        }),
+    });
+  }
+
+  return { products, isLoading, error, count, totalPages, pageNum };
+}
