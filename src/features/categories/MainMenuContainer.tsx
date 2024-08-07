738a2b057b41b5af8b10f5691a936d972cb066da
Alex-Nicalace <anicalace@gmail.com>
2024-08-07 17:34:32 +0300

    MainMenuContainer: create

---
+++ src/features/categories/MainMenuContainer.tsx
@@ -0,0 +1,41 @@
+import Spinner from '../../components/Spinner';
+import { IMenuData } from '../../data/menuData';
+import { useMainMenu } from './useMainMenu';
+
+type TMainMenuContainerProps = { children: (data: IMenuData[]) => JSX.Element };
+function MainMenuContainer({ children }: TMainMenuContainerProps): JSX.Element {
+  const { mainMenu, isLoading, categoryGender } = useMainMenu();
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  // ':categoryGender/catalog/:categoryMenu?/:categorySubmenu?'
+  const data: IMenuData[] = (mainMenu || []).map((item) => ({
+    title: item.displayName,
+    to: `${categoryGender}/catalog/${item.name}`,
+    submenu: {
+      sections: [
+        {
+          title: item.displayName,
+          isWideSection: true,
+          links: (item.subMenu || []).map((subItem) => ({
+            title: subItem.displayName,
+            to: `${categoryGender}/catalog/${item.name}/${subItem.name}`,
+          })),
+        },
+        {
+          title: 'Бренды',
+          links: (item.brands || []).map((brand) => ({
+            title: brand.name,
+            to: `${categoryGender}/catalog/${item.name}/brand/${brand.name}`,
+          })),
+        },
+      ],
+    },
+  }));
+
+  return <>{children(data)}</>;
+}
+
+export default MainMenuContainer;
