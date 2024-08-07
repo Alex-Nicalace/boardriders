5f4f3e62fe828495436c2cf23e2d2d08cca2d11a
Alex-Nicalace <anicalace@gmail.com>
2024-08-07 17:34:13 +0300

    useMainMenu: create

---
+++ src/features/categories/useMainMenu.ts
@@ -0,0 +1,22 @@
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { getMainMenu } from '../../services/apiCategories';
+import { useGenderCategoryProduct } from '../products/useGenderCategoryProduct';
+
+export function useMainMenu() {
+  const params = useParams();
+  const genderCategoryProduct = useGenderCategoryProduct();
+  const categoryGender = params.categoryGender || genderCategoryProduct || '';
+
+  const {
+    data: mainMenu,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ['mainMenu', categoryGender],
+    queryFn: () => getMainMenu(categoryGender),
+    staleTime: Infinity,
+  });
+
+  return { mainMenu, isLoading, error, categoryGender };
+}
