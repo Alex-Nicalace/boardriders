1f74430cb325ebf0c10a850e84c92edd543502b7
Alex-Nicalace <anicalace@gmail.com>
2024-08-11 18:02:01 +0300

    AboutBrand: refactoring

--- src/components/AboutBrand/AboutBrand.tsx
+++ src/components/AboutBrand/AboutBrand.tsx
@@ -4,26 +4,28 @@ import './AboutBrand.scss';
 
 type TAboutBrandProps = {
   className?: string;
-  backgroundImage: string;
-  brandIcon: string;
-  brandName: string;
-  text: string;
+  data: {
+    backgroundImage: string | null;
+    brandIcon: string | null;
+    brandName: string | null;
+    text: string | null;
+  };
 };
-function AboutBrand({
-  className,
-  backgroundImage,
-  brandIcon,
-  brandName,
-  text,
-}: TAboutBrandProps): JSX.Element {
+function AboutBrand({ className, data }: TAboutBrandProps) {
+  const { backgroundImage, brandIcon, brandName, text } = data;
+
+  if (!brandIcon && !brandName && !text) return null;
+
   return (
-    <PromoBlock className={className} backgroundImage={backgroundImage}>
+    <PromoBlock className={className} backgroundImage={backgroundImage || ''}>
       <div className="about-brand">
-        <Title className="about-brand__title" as="h1" kind="h1-32-h2-21">
-          <img src={brandIcon} alt="иконка бренда" />
-          <span>{brandName}</span>
-        </Title>
-        <p className="about-brand__text">{text}</p>
+        {(!!brandIcon || !!brandName) && (
+          <Title className="about-brand__title" as="h1" kind="h1-32-h2-21">
+            {!!brandIcon && <img src={brandIcon} alt="иконка бренда" />}
+            {!!brandName && <span>{brandName}</span>}
+          </Title>
+        )}
+        {!!text && <p className="about-brand__text">{text}</p>}
       </div>
     </PromoBlock>
   );
