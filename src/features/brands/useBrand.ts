481bc12c6248dc98c5f1d1e951e90758dce60878
Alex-Nicalace <anicalace@gmail.com>
2024-09-03 09:53:53 +0300

    useBrand: add arg "enabled"

--- src/features/brands/useBrand.ts
+++ src/features/brands/useBrand.ts
@@ -2,7 +2,13 @@ import { useQuery } from '@tanstack/react-query';
 import { getBrand } from '../../services/apiBrands';
 import { useParams } from 'react-router-dom';
 
-export function useBrand() {
+/**
+ * Используйте запрос для получения данных о бренде.
+ *
+ * @param enabled - Делать ли запрос на сервер.(по умолчанию: true)
+ * @returns - Объект с данными о бренде
+ */
+export function useBrand(enabled: boolean = true) {
   const params = useParams();
   const { brand } = params;
 
@@ -13,6 +19,7 @@ export function useBrand() {
   } = useQuery({
     queryKey: ['brand', brand],
     queryFn: () => getBrand(brand || ''),
+    enabled,
   });
 
   return { brands, isLoading, error };
