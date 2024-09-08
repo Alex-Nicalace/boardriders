e6b1e8cd7b53fa40407f9dba7e3e49a9dd4c436e
Alex-Nicalace <anicalace@gmail.com>
2024-09-08 13:20:45 +0300

    RootRedirect: usage global state

--- src/components/RootRedirect.tsx
+++ src/components/RootRedirect.tsx
@@ -1,11 +1,13 @@
 import { Navigate } from 'react-router-dom';
-import { useLocalStorageState } from '../hooks/useLocalStorageState';
 import { useGenderCategories } from '../features/categories/useCategories';
+import { useAppSelector } from '../hooks/reduxHooks';
+import { getGender } from '../features/gender/genderSlice';
 
 function RootRedirect() {
+  const genderfromLocalStorage = useAppSelector(getGender);
   const { genderCategories } = useGenderCategories();
-  const categoryGender = genderCategories && genderCategories[0].name;
-  const [redirectPath] = useLocalStorageState(categoryGender, 'categoryGender');
+  const genderDefault = genderCategories && genderCategories[0].name;
+  const redirectPath = genderfromLocalStorage || genderDefault || '';
 
   if (redirectPath) {
     return <Navigate to={`/${redirectPath}`} replace />;
