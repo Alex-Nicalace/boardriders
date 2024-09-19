724538035bb6d96fc0dd05bc36be4c067a181998
Alex-Nicalace <anicalace@gmail.com>
2024-09-19 16:21:48 +0300

    useLogin: create

---
+++ src/features/authentication/useLogin.ts
@@ -0,0 +1,25 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
+import { login as loginApi, TLoginParams } from '../../services/apiAuth';
+
+export function useLogin() {
+  const queryClient = useQueryClient();
+  const { mutate: login, isPending: isLogging } = useMutation({
+    mutationFn: ({ email, password }: TLoginParams) => {
+      return loginApi({ email, password });
+    },
+    onSuccess: (user) => {
+      // в компоненте ProtectedRoute происходит запрос получения данных о пользователе и помещение этих данных в кэш
+      // это проиходит с некоторой периодичностью и тем самым происходит проверка авторизации. есть смысл после аунтификации
+      // сразу поместить данные о пользователе в кэш чтобы не делать лишний запрос за запросом
+      console.log('Пользователь успешно вошел!');
+      queryClient.setQueryData(['user'], user.user);
+    },
+    onError: (err) => {
+      console.error(err);
+      toast.error(`Во время входа произошла ошибка! ${err.message}`);
+    },
+  });
+
+  return { login, isLogging };
+}
