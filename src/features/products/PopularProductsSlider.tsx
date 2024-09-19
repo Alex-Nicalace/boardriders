45f4544d2cde7056e6894325a778c6ac141c18d7
Alex-Nicalace <anicalace@gmail.com>
2024-09-19 14:54:03 +0300

    PopularProductsSlider: usage ErrorMessage

--- src/features/products/PopularProductsSlider.tsx
+++ src/features/products/PopularProductsSlider.tsx
@@ -1,4 +1,4 @@
-import Empty from '../../components/Empty';
+import ErrorMessage from '../../components/ErrorMessage';
 import Spinner from '../../components/Spinner';
 import SliderWareCards from '../../components/ui/SliderWareCards';
 import { usePopularProducts } from './usePopularProducts';
@@ -6,15 +6,20 @@ import { usePopularProducts } from './usePopularProducts';
 type TNewProductsSliderProps = {
   className?: string;
 };
-function PopularProductsSlider({
-  className,
-}: TNewProductsSliderProps): JSX.Element {
-  const { popularProducts, isLoading } = usePopularProducts(15);
+function PopularProductsSlider({ className }: TNewProductsSliderProps) {
+  const { popularProducts, isLoading, error } = usePopularProducts(15);
 
-  if (isLoading) return <Spinner />;
+  if (isLoading) return <Spinner className={className} />;
 
-  if (!popularProducts || !popularProducts.length)
-    return <Empty resource="Топ продаж" />;
+  if (error)
+    return (
+      <ErrorMessage
+        className={className}
+        message="Не удалось загрузить популярные товары"
+      />
+    );
+
+  if (!popularProducts || !popularProducts.length) return null;
 
   return <SliderWareCards className={className} data={popularProducts} />;
 }
