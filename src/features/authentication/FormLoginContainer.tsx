05dfba62f0ab2471a5bea93f12e5b4a5b0af328c
Alex-Nicalace <anicalace@gmail.com>
2024-09-19 14:51:35 +0300

    FormLoginContainer: create

---
+++ src/features/authentication/FormLoginContainer.tsx
@@ -0,0 +1,27 @@
+import {
+  FormLogin,
+  TFormLoginProps,
+  TInputsLogin,
+} from '../../components/FormAuth';
+import { useLogin } from './useLogin';
+
+type TFormLoginContainerProps = Omit<
+  TFormLoginProps,
+  'disabled' | 'onSubmit'
+> & {
+  onSuccessLogin?: () => void;
+};
+function FormLoginContainer({
+  onSuccessLogin,
+  ...props
+}: TFormLoginContainerProps): JSX.Element {
+  const { login, isLogging } = useLogin();
+
+  function onSubmit({ email, password }: TInputsLogin) {
+    login({ email, password }, { onSuccess: onSuccessLogin });
+  }
+
+  return <FormLogin {...props} onSubmit={onSubmit} disabled={isLogging} />;
+}
+
+export default FormLoginContainer;
