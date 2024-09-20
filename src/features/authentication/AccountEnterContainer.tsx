945adeb48839cbce98fa99f68c1f670edd1b9f7a
Alex-Nicalace <anicalace@gmail.com>
2024-09-20 14:53:55 +0300

    AccountEnterContainer: usage useLogout

--- src/features/authentication/AccountEnterContainer.tsx
+++ src/features/authentication/AccountEnterContainer.tsx
@@ -1,17 +1,19 @@
 import AccountEnter from '../../components/AccountEnter';
+import { useLogout } from './useLogout';
 import { useUser } from './useUser';
 
 type TAccountEnterContainerProps = { className?: string };
 function AccountEnterContainer({
   className,
 }: TAccountEnterContainerProps): JSX.Element {
+  const { logout } = useLogout();
   const { isAuthenticated, user } = useUser();
   const name =
     isAuthenticated && user
       ? user.user_metadata.fullName?.split(' ')?.[0]
       : null;
 
-  return <AccountEnter className={className} name={name} />;
+  return <AccountEnter className={className} name={name} onExit={logout} />;
 }
 
 export default AccountEnterContainer;
