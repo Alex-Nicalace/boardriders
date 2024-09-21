36bff6234696ca271887c1b74e2fc5cbd5c05d7b
Alex-Nicalace <anicalace@gmail.com>
2024-09-21 19:07:05 +0300

    ButtonWishList: usage hooks favorite

--- src/features/wishList/ButtonWishList/ButtonWishList.tsx
+++ src/features/wishList/ButtonWishList/ButtonWishList.tsx
@@ -1,6 +1,11 @@
+import toast from 'react-hot-toast';
 import { addWishList, getWishList, removeWishList } from '../wishListSlise';
 import { useAppDispatch, useAppSelector } from '../../../hooks/reduxHooks';
 import Favorite, { TFavoriteProps } from '../../../components/ui/Favorite';
+import { useUser } from '../../authentication/useUser';
+import { useFavorites } from '../../favorites/useFavorites';
+import { useCreateFavorites } from '../../favorites/useCreateFavorites';
+import { useDeleteFavorites } from '../../favorites/useDeleteFavorites';
 
 type TButtonWishListProps = Omit<
   TFavoriteProps,
@@ -10,15 +15,48 @@ function ButtonWishList({
   productId,
   ...props
 }: TButtonWishListProps): JSX.Element {
+  const { isAuthenticated, user } = useUser();
+  const { id: userId } = user || {};
+
+  const { favoritesIds } = useFavorites(isAuthenticated);
+  const { createFavorites } = useCreateFavorites();
+  const { deleteFavorites } = useDeleteFavorites();
+
   const wishList = useAppSelector(getWishList);
   const dispatch = useAppDispatch();
-  const isWished = wishList.includes(productId);
 
-  function handleChange(id: number) {
-    if (isWished) {
-      dispatch(removeWishList(id));
+  const isWished =
+    isAuthenticated && favoritesIds
+      ? favoritesIds.has(productId)
+      : wishList.includes(productId);
+
+  function deleteWishList(productId: number) {
+    if (isAuthenticated) {
+      deleteFavorites(productId);
     } else {
-      dispatch(addWishList(id));
+      dispatch(removeWishList(productId));
+    }
+  }
+
+  function createWishList(productId: number) {
+    if (isAuthenticated) {
+      if (!userId) {
+        toast.error(
+          'Не удалось добавить в избранное, т.к. нет данных пользователя!'
+        );
+        return;
+      }
+      createFavorites({ productId, userId });
+    } else {
+      dispatch(addWishList(productId));
+    }
+  }
+
+  function handleChange(productId: number) {
+    if (isWished) {
+      deleteWishList(productId);
+    } else {
+      createWishList(productId);
     }
   }
   return (
