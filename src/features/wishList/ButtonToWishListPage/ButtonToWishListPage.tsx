07355ded806c55aadb5bc889866d75607c75edd7
Alex-Nicalace <anicalace@gmail.com>
2024-09-21 19:05:25 +0300

    ButtonToWishListPage: usage hooks favorite

--- src/features/wishList/ButtonToWishListPage/ButtonToWishListPage.tsx
+++ src/features/wishList/ButtonToWishListPage/ButtonToWishListPage.tsx
@@ -1,14 +1,21 @@
 import IconButton from '../../../components/ui/IconButton';
 import { StarIcon } from '../../../components/ui/Icons';
 import { useAppSelector } from '../../../hooks/reduxHooks';
+import { useUser } from '../../authentication/useUser';
+import { useFavorites } from '../../favorites/useFavorites';
 import { getWishList } from '../wishListSlise';
 
 type TButtonToWishListPageProps = { className?: string };
 function ButtonToWishListPage({
   className,
 }: TButtonToWishListPageProps): JSX.Element {
-  const wishList = useAppSelector(getWishList);
-  const count = wishList.length;
+  const { isAuthenticated } = useUser();
+  const { count: wishListRemote } = useFavorites(isAuthenticated);
+
+  const wishListLocal = useAppSelector(getWishList);
+
+  const count =
+    isAuthenticated && wishListRemote ? wishListRemote : wishListLocal.length;
 
   return (
     <IconButton
