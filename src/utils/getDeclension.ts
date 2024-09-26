f46a3b4e383b600452f6fa20a38ff4a463170d14
Alex-Nicalace <anicalace@gmail.com>
2024-09-26 17:01:54 +0300

    getDeclension: create

---
+++ src/utils/getDeclension.ts
@@ -0,0 +1,18 @@
+export function getDeclension(
+  count: number,
+  one: string,
+  few: string,
+  many: string
+) {
+  if (count % 10 === 1 && count % 100 !== 11) {
+    return one; // 1 товар
+  } else if (
+    count % 10 >= 2 &&
+    count % 10 <= 4 &&
+    (count % 100 < 10 || count % 100 >= 20)
+  ) {
+    return few; // 2-4 товара
+  } else {
+    return many; // 5+ товаров
+  }
+}
