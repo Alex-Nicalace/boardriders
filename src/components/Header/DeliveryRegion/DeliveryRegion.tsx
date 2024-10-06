23d0eb6d5a2ec9975f9ef35853f39f247e4cc5ec
Alex-Nicalace <anicalace@gmail.com>
2024-10-06 11:58:06 +0300

    DeliveryRegion: usage DeliveryRegionListProvider

--- src/components/Header/DeliveryRegion/DeliveryRegion.tsx
+++ src/components/Header/DeliveryRegion/DeliveryRegion.tsx
@@ -1,6 +1,7 @@
 import { LocationIcon, SelectIcon } from '../../ui/Icons';
 import './DeliveryRegion.scss';
 import Select from '../../../component-library/Select';
+import DeliveryRegionListProvider from '../../../features/deliveryRegion/DeliveryRegionListProvider';
 
 interface IDeliveryRegionProps {
   className?: string;
@@ -8,24 +9,30 @@ interface IDeliveryRegionProps {
 
 function DeliveryRegion({ className }: IDeliveryRegionProps): JSX.Element {
   return (
-    <div className={`${className} delivery-region`}>
+    <div className={['delivery-region', className].filter(Boolean).join(' ')}>
       <label htmlFor="delivery-region" className="delivery-region__label">
         <LocationIcon className="delivery-region__icon" />
         <span className="delivery-region__text">Ваш регион доставки:</span>
       </label>
-      <Select
-        className="delivery-region__select"
-        name="delivery-region"
-        id="delivery-region"
-        // value={city}
-        initValue="20"
-        // onChange={handleChangeCity}
-        iconSelect={<SelectIcon />}
-      >
-        <Select.Option value="10">Москва</Select.Option>
-        <Select.Option value="20">Санкт-Петербург</Select.Option>
-        <Select.Option value="30">Пенза</Select.Option>
-      </Select>
+      <DeliveryRegionListProvider
+        render={(deliveryRegionList) => (
+          <Select
+            className="delivery-region__select"
+            name="delivery-region"
+            id="delivery-region"
+            // value={city}
+            initValue="1"
+            // onChange={handleChangeCity}
+            iconSelect={<SelectIcon />}
+          >
+            {deliveryRegionList.map((item) => (
+              <Select.Option key={item.id} value={`${item.id}`}>
+                {item.name}
+              </Select.Option>
+            ))}
+          </Select>
+        )}
+      />
     </div>
   );
 }
