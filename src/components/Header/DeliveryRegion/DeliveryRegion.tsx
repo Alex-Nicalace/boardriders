d0adcc58c6f3d01569acb6d6c6d2d763c4b5519c
Alex-Nicalace <anicalace@gmail.com>
2024-10-07 18:07:05 +0300

    DeliveryRegion: add props

--- src/components/Header/DeliveryRegion/DeliveryRegion.tsx
+++ src/components/Header/DeliveryRegion/DeliveryRegion.tsx
@@ -1,37 +1,40 @@
 import { LocationIcon, SelectIcon } from '../../ui/Icons';
 import './DeliveryRegion.scss';
 import Select from '../../../component-library/Select';
-import DeliveryRegionListProvider from '../../../features/deliveryRegion/DeliveryRegionListProvider';
 
 interface IDeliveryRegionProps {
   className?: string;
+  selected: string;
+  list: { id: number; name: string }[];
+  onChange: (id: string) => void;
 }
 
-function DeliveryRegion({ className }: IDeliveryRegionProps): JSX.Element {
+function DeliveryRegion({
+  className,
+  selected,
+  list,
+  onChange,
+}: IDeliveryRegionProps): JSX.Element {
   return (
     <div className={['delivery-region', className].filter(Boolean).join(' ')}>
       <label htmlFor="delivery-region" className="delivery-region__label">
         <LocationIcon className="delivery-region__icon" />
         <span className="delivery-region__text">Ваш регион доставки:</span>
       </label>
-      <DeliveryRegionListProvider
-        render={(deliveryRegionList, currentDeliveryRegionId, handleChange) => (
-          <Select
-            className="delivery-region__select"
-            name="delivery-region"
-            id="delivery-region"
-            value={`${currentDeliveryRegionId}`}
-            onChange={(value) => handleChange?.(Number(value))}
-            iconSelect={<SelectIcon />}
-          >
-            {deliveryRegionList.map((item) => (
-              <Select.Option key={item.id} value={`${item.id}`}>
-                {item.name}
-              </Select.Option>
-            ))}
-          </Select>
-        )}
-      />
+      <Select
+        className="delivery-region__select"
+        name="delivery-region"
+        id="delivery-region"
+        value={selected}
+        onChange={onChange}
+        iconSelect={<SelectIcon />}
+      >
+        {list.map((item) => (
+          <Select.Option key={item.id} value={`${item.id}`}>
+            {item.name}
+          </Select.Option>
+        ))}
+      </Select>
     </div>
   );
 }
