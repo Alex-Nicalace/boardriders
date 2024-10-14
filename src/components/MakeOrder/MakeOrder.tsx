0e312cbd3dca38556e32c19391bdef8ee9cbf635
Alex-Nicalace <anicalace@gmail.com>
2024-10-14 09:40:43 +0300

    MakeOrder: refactoring

--- src/components/MakeOrder/MakeOrder.tsx
+++ src/components/MakeOrder/MakeOrder.tsx
@@ -1,45 +1,53 @@
 import './MakeOrder.scss';
-import DeliveryOption from '../DeliveryOption';
 import MakeOrderStep from '../MakeOrderStep';
 import PaymentOption from '../PaymentOption';
+import DeliveryOptionContainer from '../../features/delivery/DeliveryOptionContainer';
+import ContactOption from '../ContactOption';
+import DeliveryCourierData from '../DeliveryCourierData';
 import ContactBuyerData from '../ContactBuyerData';
 
-// type TMakeOrderProps = { }
-function MakeOrder(/*{ }: TMakeOrderProps*/): JSX.Element {
+const ELEMENTS = [
+  [
+    <DeliveryOptionContainer />,
+    <DeliveryCourierData
+      street="ул. Ленина"
+      building="д. 1"
+      dateDelivery={new Date()}
+      timeDelivery="10:00"
+      apartment="1"
+      entrance="1"
+      floor="2"
+    />,
+  ],
+  [<PaymentOption />, <PaymentOption />],
+  [
+    <ContactOption />,
+    <ContactBuyerData
+      name="Иванов Иван Иванович"
+      phone="+7 (495) 967 96 23"
+      email="Xjz6H@example.com"
+      comment="Пожалуйста, мне нужен товар в целой коробке, беру на подарок"
+    />,
+  ],
+];
+type TMakeOrderProps = {
+  stepsData: { nameStep: string; isDone: boolean; disabled: boolean }[];
+};
+function MakeOrder({ stepsData }: TMakeOrderProps): JSX.Element {
   return (
     <ol className="make-order">
-      <li className="make-order__step">
-        <MakeOrderStep stepNum={1} name="Доставка">
-          <DeliveryOption />
-          {/* когда данные введены пользователем рендерить этот компонент */}
-          {/* <DeliveryCourierData
-            street="ул. Ленина"
-            building="д. 1"
-            dateDelivery={new Date()}
-            timeDelivery="10:00"
-            apartment="1"
-            entrance="1"
-            floor="2"
-          /> */}
-        </MakeOrderStep>
-      </li>
-      <li className="make-order__step">
-        <MakeOrderStep stepNum={2} name="Оплата">
-          <PaymentOption />
-        </MakeOrderStep>
-      </li>
-      <li className="make-order__step">
-        <MakeOrderStep stepNum={3} name="Контакты">
-          {/* <ContactOption /> */}
-          {/* когда данные введены пользователем рендерить этот компонент */}
-          <ContactBuyerData
-            name="Иванов Иван Иванович"
-            phone="+7 (495) 967 96 23"
-            email="Xjz6H@example.com"
-            comment="Пожалуйста, мне нужен товар в целой коробке, беру на подарок"
-          />
-        </MakeOrderStep>
-      </li>
+      {stepsData.map((item, index) => (
+        <li key={item.nameStep} className="make-order__step">
+          <MakeOrderStep
+            stepNum={index + 1}
+            name={item.nameStep}
+            isDone={item.isDone}
+            disabled={item.disabled}
+          >
+            {ELEMENTS[index][+item.isDone]}
+          </MakeOrderStep>
+        </li>
+      ))}
     </ol>
   );
 }
