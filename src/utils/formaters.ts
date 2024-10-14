936653fe463d99e2d04dfc9c0a281e352038e191
Alex-Nicalace <anicalace@gmail.com>
2024-10-14 09:36:39 +0300

    formaters: create

---
+++ src/utils/formaters.ts
@@ -0,0 +1,32 @@
+export const formaterCurrency = new Intl.NumberFormat('ru-RU', {
+  style: 'currency',
+  currency: 'RUB',
+  minimumFractionDigits: 0,
+}).format;
+
+export const formaterPercent = new Intl.NumberFormat('ru-RU', {
+  style: 'percent',
+}).format;
+
+export const formaterDate = new Intl.DateTimeFormat('ru', {
+  day: 'numeric',
+  month: 'long',
+  year: 'numeric',
+}).format;
+
+export const formaterDateShort = new Intl.DateTimeFormat('ru', {
+  day: 'numeric',
+  month: '2-digit',
+  year: 'numeric',
+}).format;
+
+export const formaterDateWithWeekday = new Intl.DateTimeFormat('ru', {
+  day: 'numeric',
+  month: 'long',
+  weekday: 'short',
+}).format;
+
+export const formaterDecimal = new Intl.NumberFormat('ru-RU', {
+  style: 'decimal',
+  minimumFractionDigits: 0, // не показывать друбную часть, если ее нет
+}).format;
