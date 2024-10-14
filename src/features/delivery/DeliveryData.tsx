6acbda462a2c6f0849429f26aa8aeffab2aa555d
Alex-Nicalace <anicalace@gmail.com>
2024-10-14 16:27:01 +0300

    DeliveryData: create

---
+++ src/features/delivery/DeliveryData.tsx
@@ -0,0 +1,32 @@
+import DeliveryCourierData from '../../components/DeliveryCourierData';
+import ErrorMessage from '../../components/ErrorMessage';
+import { useAppSelector } from '../../hooks/reduxHooks';
+import { getOrderDataOnStep } from '../makeOrder/makeOrderSlice';
+
+function DeliveryData(): JSX.Element {
+  const deliveryData = useAppSelector(getOrderDataOnStep(0));
+
+  if (deliveryData.deliveryMethod === 'courier' && deliveryData.courier) {
+    const { street, building, apartment, entrance, floor, date, time } =
+      deliveryData.courier;
+    return (
+      <DeliveryCourierData
+        street={street}
+        building={building}
+        dateDelivery={new Date(date)}
+        timeDelivery={time}
+        apartment={apartment}
+        entrance={entrance}
+        floor={floor}
+      />
+    );
+  }
+
+  // if (deliveryData.deliveryMethod === 'delivery-in-shop' && deliveryData['delivery-in-shop']) {
+  //   // компонент, который показывает введнные пользователем данные о доставке данным способом
+  // }
+
+  return <ErrorMessage message="Не предусмотренный способ доставки!" />;
+}
+
+export default DeliveryData;
