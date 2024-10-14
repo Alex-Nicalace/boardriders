b37d0de37e7c799dfdaa4aa7c1ef1449b962c5e1
Alex-Nicalace <anicalace@gmail.com>
2024-10-14 09:39:39 +0300

    DeliveryOptionContainer: add handle event submit

--- src/features/delivery/DeliveryOptionContainer.tsx
+++ src/features/delivery/DeliveryOptionContainer.tsx
@@ -1,17 +1,29 @@
-import DeliveryOption from '../../components/DeliveryOption';
+import DeliveryOption, { TDeliveryForm } from '../../components/DeliveryOption';
 import ErrorMessage from '../../components/ErrorMessage';
 import Spinner from '../../components/Spinner';
+import { useAppDispatch } from '../../hooks/reduxHooks';
+import { setOrderFirstStep } from '../makeOrder/makeOrderSlice';
 import { useDeliveryRegionList } from './useDeliveryRegionList';
 
 // type TDeliveryOptionContainerProps = { }
 function DeliveryOptionContainer(/*{ }: TDeliveryOptionContainerProps*/): JSX.Element {
   const { deliveryRegionList, isLoading, error } = useDeliveryRegionList();
+  const dispatch = useAppDispatch();
 
   if (isLoading) return <Spinner />;
 
   if (error) return <ErrorMessage message={error.message} />;
 
-  return <DeliveryOption deliveryRegionList={deliveryRegionList || []} />;
+  function handleSubmit(data: TDeliveryForm) {
+    dispatch(setOrderFirstStep(data));
+  }
+
+  return (
+    <DeliveryOption
+      deliveryRegionList={deliveryRegionList || []}
+      onSubmit={handleSubmit}
+    />
+  );
 }
 
 export default DeliveryOptionContainer;
