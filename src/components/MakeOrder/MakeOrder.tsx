01f482f4eba3bb8141127204c1cf2f0fe159fd23
Alex-Nicalace <anicalace@gmail.com>
2024-10-15 13:31:01 +0300

    MakeOrder: usage ContactOptionContainer, ContactBuyerDataContainer

--- src/components/MakeOrder/MakeOrder.tsx
+++ src/components/MakeOrder/MakeOrder.tsx
@@ -1,24 +1,16 @@
 import './MakeOrder.scss';
 import MakeOrderStep from '../MakeOrderStep';
 import DeliveryOptionContainer from '../../features/makeOrder/DeliveryOptionContainer';
-import ContactOption from '../ContactOption';
-import ContactBuyerData from '../ContactBuyerData';
 import DeliveryData from '../../features/makeOrder/DeliveryData';
 import PaymentOptionContainer from '../../features/makeOrder/PaymentOptionContainer';
 import { TGetMakeOrderStepsReturn } from '../../types';
+import ContactBuyerDataContainer from '../../features/makeOrder/ContactBuyerDataContainer';
+import ContactOptionContainer from '../../features/makeOrder/ContactOptionContainer';
 
 const ELEMENTS = [
   [<DeliveryOptionContainer />, <DeliveryData />],
   [<PaymentOptionContainer />, null],
-  [
-    <ContactOption />,
-    <ContactBuyerData
-      name="Иванов Иван Иванович"
-      phone="+7 (495) 967 96 23"
-      email="Xjz6H@example.com"
-      comment="Пожалуйста, мне нужен товар в целой коробке, беру на подарок"
-    />,
-  ],
+  [<ContactOptionContainer />, <ContactBuyerDataContainer />],
 ];
 type TMakeOrderProps = {
   stepsData: TGetMakeOrderStepsReturn[];
@@ -31,7 +23,9 @@ function MakeOrder({ stepsData, editOnStep }: TMakeOrderProps): JSX.Element {
         <li key={item.nameStep} className="make-order__step">
           <MakeOrderStep
             stepNum={index + 1}
-            name={item.isDone ? item.donedNameStep : item.nameStep}
+            name={
+              item.isDone ? item.donedNameStep || item.nameStep : item.nameStep
+            }
             isDone={item.isDone}
             disabled={item.disabled}
             onClickEditStep={() => editOnStep?.(index)}
