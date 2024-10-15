98afc38ae54f6bc3b0affd83792973ae8af439da
Alex-Nicalace <anicalace@gmail.com>
2024-10-15 13:30:31 +0300

    ContactOption: add props

--- src/components/ContactOption/ContactOption.tsx
+++ src/components/ContactOption/ContactOption.tsx
@@ -1,13 +1,28 @@
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import './ContactOption.scss';
 import InputStyled from '../ui/InputStyled';
 import Checkbox from '../ui/Checkbox';
 import Button from '../ui/Button';
+import { TContactsData } from '../../types';
+import { MSG_REQUIRED } from '../FormAuth/constants';
 
-// type TContactOptionProps = { }
-function ContactOption(/*{ }: TContactOptionProps*/): JSX.Element {
+type TContactOptionProps = {
+  defaultValues?: TContactsData;
+  onSubmit?: (data: TContactsData) => void;
+};
+function ContactOption({
+  defaultValues,
+  onSubmit = () => {},
+}: TContactOptionProps): JSX.Element {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TContactsData>({ defaultValues });
+
   return (
-    <div className="contact-option">
+    <form className="contact-option" onSubmit={handleSubmit(onSubmit)}>
       <p className="contact-option__invitation">
         Укажите свои данные, чтобы быть в курсе изменений статуса заказа.
         Персональные данные обрабатываются в соответствии с{' '}
@@ -16,13 +31,22 @@ function ContactOption(/*{ }: TContactOptionProps*/): JSX.Element {
         </Link>
       </p>
 
-      <InputStyled className="contact-option__name" label="Имя" isGrayLabel />
+      <InputStyled
+        className="contact-option__name"
+        label="Имя"
+        isGrayLabel
+        {...register('name', { required: MSG_REQUIRED })}
+        error={errors.name?.message}
+      />
 
       <InputStyled
         className="contact-option__phone"
         label="Телефон для SMS уведомлений"
         placeholder="+7 ( ХХХ) ХХХ ХХ ХХ"
         isGrayLabel
+        type="tel"
+        {...register('phone', { required: MSG_REQUIRED })}
+        error={errors.phone?.message}
       />
 
       <InputStyled
@@ -30,9 +54,16 @@ function ContactOption(/*{ }: TContactOptionProps*/): JSX.Element {
         label="Email"
         placeholder="Введите ваш Email"
         isGrayLabel
+        type="email"
+        {...register('email', { required: MSG_REQUIRED })}
+        error={errors.email?.message}
       />
       <div className="contact-option__mailing">
-        <Checkbox label="Подписаться на рассылку" variantIcon="square" />
+        <Checkbox
+          label="Подписаться на рассылку"
+          variantIcon="square"
+          {...register('isSubscribed')}
+        />
       </div>
 
       <InputStyled
@@ -41,12 +72,13 @@ function ContactOption(/*{ }: TContactOptionProps*/): JSX.Element {
         placeholder="Нипишите что - нибудь"
         isGrayLabel
         isTextarea
+        {...register('comment')}
       />
 
       <div className="contact-option__btn-confirm">
         <Button fullWidth>Подтвердить</Button>
       </div>
-    </div>
+    </form>
   );
 }
 
