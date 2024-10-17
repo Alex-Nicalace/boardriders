57b41e15f3e697428de62efe9e231c9eafb9e862
Alex-Nicalace <anicalace@gmail.com>
2024-10-17 16:59:06 +0300

    useCart: add get remote data

--- src/features/cart/cart.types.ts
+++ src/features/cart/cart.types.ts
@@ -1,3 +1,10 @@
-import { getProductVariants } from '../../services/apiProducts';
+import { getCartProducts } from '../../services/apiCart/getCartProducts';
 
-export type TCartList = Awaited<ReturnType<typeof getProductVariants>>;
+export type TCartList = Awaited<ReturnType<typeof getCartProducts>>;
+
+export type TCartItem = Omit<
+  Awaited<ReturnType<typeof getCartProducts>>[number],
+  'cartId'
+> & {
+  cartId?: number;
+};
--- src/features/cart/useCart.ts
+++ src/features/cart/useCart.ts
@@ -1,29 +1,44 @@
-import { useMemo } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { getProductVariants } from '../../services/apiProducts';
 import { useAppSelector } from '../../hooks/reduxHooks';
 import { getCartIds, getCartMapping } from './cartSlice';
+import { useUser } from '../authentication/useUser';
+import { getCartProducts } from '../../services/apiCart/getCartProducts';
+import { TCartItem } from './cart.types';
 
 export function useCart(enabled = true) {
   const productVariantIds = useAppSelector(getCartIds);
   const cartMapping = useAppSelector(getCartMapping);
+  const { isAuthenticated } = useUser();
 
-  const { data, isLoading, error } = useQuery({
-    queryKey: ['cart'],
-    queryFn: () => getProductVariants(productVariantIds),
+  /**
+   * Получение товаров и сопоставление с количеством
+   */
+  const getCartFromLocalStorage = async () => {
+    const data = await getProductVariants(productVariantIds);
+    return data.map((product) => {
+      return {
+        ...product,
+        quantity: cartMapping[product.productVariantId].count,
+      };
+    });
+  };
+
+  /**
+   * Получение товаров из удлаленной корзины или из локального хранилища
+   */
+  const queryFn: () => Promise<TCartItem[]> = async () =>
+    isAuthenticated ? getCartProducts() : getCartFromLocalStorage();
+
+  const {
+    data: products,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ['cart', isAuthenticated],
+    queryFn,
     enabled,
   });
-
-  const products = useMemo(
-    () =>
-      data?.map((product) => {
-        return {
-          ...product,
-          quantity: cartMapping[product.id].count,
-        };
-      }),
-    [data, cartMapping]
-  );
 
   const { quantityTotal, priceTotal } = products?.reduce(
     (acc, item) => ({
