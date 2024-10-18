4fb29dcbb07f0766f1a7a8f8138a2ed2648ae2ca
Alex-Nicalace <anicalace@gmail.com>
2024-10-18 12:29:58 +0300

    ButtonToCartPage: usage useTotalItemsCart

--- src/features/cart/ButtonToCartPage.tsx
+++ src/features/cart/ButtonToCartPage.tsx
@@ -1,20 +1,19 @@
 import IconButton from '../../components/ui/IconButton';
 import { CartIcon } from '../../components/ui/Icons';
-import { useAppSelector } from '../../hooks/reduxHooks';
-import { getTotalQuantity } from './cartSlice';
+import { useTotalItemsCart } from './useTotalItemsCart';
 
 type TButtonToCartPageProps = {
   className?: string;
 };
 function ButtonToCartPage({ className }: TButtonToCartPageProps) {
-  const totalQuantity = useAppSelector(getTotalQuantity);
+  const { totalItemsCart } = useTotalItemsCart();
 
   return (
     <IconButton
       className={className}
       IconComponent={CartIcon}
       to="/cart"
-      badgeCounter={totalQuantity || undefined}
+      badgeCounter={totalItemsCart || undefined}
     >
       Корзина
     </IconButton>
